5ba3ef5c173634c038699c1b445a752d4ccf9470
Brennen Fagan <brennen.fagan@york.ac.uk>
2021-04-23 09:31:41 +0100

    Created a full example run of how islands of communities might interact.

--- R/CommunityIslands-ExampleRun.R
+++ R/CommunityIslands-ExampleRun.R
@@ -1,4 +1,18 @@
+# Settings. ####################################################################
+islands <- c(2, 1) # Community Numbers
+
+Tolerance <- 1E-6
+
+OuterTimeStepSize <- 2.5E3
+InnerTimeStepSize <- 1E1
+
+ts <- seq(from = 0,
+          to = OuterTimeStepSize,
+          by = InnerTimeStepSize)
+
 # Recreate the communities. ####################################################
+library(RMTRCode2)
+
 set.seed(3680180)
 
 basal2 <- c(5, 10, 15)
@@ -11,7 +25,7 @@ seedsPrep2 <- runif(2 * length(basal2) * length(consumer2)) * 1E8
 pool <- with(list(
   b = rep(basal2, times = length(consumer2)),
   c = rep(consumer2, each = length(basal2)),
-  s = seedsPrep2[1:(length(basal2) * length(consumer2))],
+  s = seedsPrep2[1:(length(basal2) * length(consumer2))]
   ),
   LawMorton1996_species(
     Basal = b[6],
@@ -31,7 +45,7 @@ comMat <- with(list(
   LawMorton1996_CommunityMat(
     Pool = pool,
     Parameters = parameters,
-    seed = s
+    seed = s[6]
   )
 )
 
@@ -41,46 +55,78 @@ communities <- c(
   "8, 12, 14, 15, 16, 38, 39"
 )
 
+inits <- c(
+  "21, 32, 80, 818, 121, 18, 12, 20",
+  "83, 138, 938, 52, 5, 14, 26"
+)
+
+stopifnot(length(communities) == length(inits))
+
 # > runif(1) * 1E8
 # [1] 20649322
 set.seed(20649322)
 
-populations <- lapply(communities, function(strCom) {
-  # Convert to indices.
-  com <- as.numeric(
-    unlist(strsplit(strCom, split = ", "))
-  )
-
-  # Attempt to calculate the (assumed) stable steady state.
-  # Make a guess.
-  init <- runif(n = length(com), min = 1, max = 1000)
-
-  anyZeroOrNotSame <- TRUE # Set T to make at least one look.
-
-  # Run and check to see if anyone dies (bad) or changes (not at steady).
-  while (anyZeroOrNotSame) {
-    init_old <- init
-    init <- LawMorton1996_NumIntegration(
-      A = comMat[com, com],
-      R = pool$ReproductionRate[com],
-      X = init,
-      Tolerance = 1E-10
+populations <- lapply(
+  seq_along(communities),
+  function(i, communities, inits) {
+    # Convert to indices.
+    com <- as.numeric(
+      unlist(strsplit(communities[i], split = ", "))
+    )
+    init <- as.numeric(
+      unlist(strsplit(inits[i], split = ", "))
     )
 
-    if (any(init == 0)) {
-      # Someone died, reset to random location.
-      anyZeroOrNotSame <- TRUE
-      init <- runif(n = length(com), min = 1, max = 1000)
-    } else if (any(round(init / init_old) != 1)) {
-      # Not done, keep going.
-      anyZeroOrNotSame <- TRUE
-    } else {
-      anyZeroOrNotSame <- FALSE
-    }
-  }
+    anyZeroOrNotSame <- TRUE # Set T to make at least one look.
+    epsilon <- 1E-1
 
-  return(init)
-})
+    # Run and check to see if anyone dies (bad) or changes (not at steady).
+    while (anyZeroOrNotSame) {
+      init_old <- init
+      # Initial attempt: random search plus dynamics.
+      # init <- LawMorton1996_NumIntegration(
+      #   A = comMat[com, com],
+      #   R = pool$ReproductionRate[com],
+      #   X = init,
+      #   OuterTimeStepSize = 1000,
+      #   InnerTimeStepSize = 1,
+      #   Tolerance = 1E-10
+      # )
+      # init <- init[nrow(init),]
+      # init <- init[-1]
+      #
+      # Applying a more specialised tool.
+      init <- rootSolve::steady(
+        y = init,
+        func = GeneralisedLotkaVolterra,
+        parms = list(a = comMat[com, com],
+                     r = pool$ReproductionRate[com],
+                     epsilon = epsilon),
+        positive = TRUE
+      )$y
+      print(init)
+
+      if (any(init < epsilon)) {
+        # Someone died, reset to random location.
+        print("Died")
+        anyZeroOrNotSame <- TRUE
+        init[init < epsilon] <- runif(n = sum(init < epsilon), min = 2, max = 10000)
+      } else if (any(round(init / init_old, 1) != 1)) {
+        # Not done, keep going.
+        #print(init / init_old)
+        #print(round(init / init_old, 1))
+        print("Changed")
+        anyZeroOrNotSame <- TRUE
+      } else {
+        anyZeroOrNotSame <- FALSE
+      }
+    }
+
+    return(init)
+  },
+  communities = communities,
+  inits = inits
+)
 
 # Reduce to necessary information. #############################################
 # Total list of species present.
@@ -88,57 +134,68 @@ redCom <- sort(unique(as.numeric(
   unlist(lapply(communities, strsplit, split = ", "))
 )))
 
-redPool <- pool[redCom]
+redPool <- pool[redCom,]
 redComMat <- comMat[redCom, redCom]
 
 # Convert to new indices
-redComs <- lapply(communities, function(strCom) {
+redComs <- lapply(communities[islands], function(strCom) {
   which(redCom %in% as.numeric(
     unlist(strsplit(strCom, split = ", "))
   ))
 })
 
+redPops <- populations[islands]
+
 # Sanity Check: Lengths should match.
 stopifnot(all(
-  unlist(lapply(populations, length)) == unlist(lapply(redComs, length))
+  unlist(lapply(redPops, length)) == unlist(lapply(redComs, length))
 ))
 
 # Evaluation Parameters. #######################################################
-# Amount of travel from island i to island j is [i, j].
-# Amount of gain from island i to island j is [j, i].
+# Amount of travel from j to i is d[i,j]
+# Amount of gain to i from j is d[i,j]
+# Amount of travel from i is d[i,i]
+# This way, we can write the change in y from travel as d %*% y
 # Characterising this as proportions of a population, but that is assuming a
 # normalisation that I do not think is strictly necessary.
-dispersalMatrix <- matrix(c(
-  0.9, 0.1,
-  0.1, 0.9
-), byrow = TRUE)
+# The matrix is sparse and has colsum = 0, diag < 0, offdiag >= 0.
+dispersalMatrix <- Matrix::bandSparse(
+  n = length(redCom) * length(islands),
+  k = length(redCom) * c(1:(length(islands) - 1),
+                         -(1:(length(islands) - 1))),
+  diagonals = c(
+    list(rep(0.01, length(redCom))), # Island 2 -> Island 1
+    list(rep(0.01, length(redCom))) # Island 1 -> Island 2
+  )
+)
+#   matrix(c(
+#   0.9, 0.1,
+#   0.01, 0.99
+# ), byrow = TRUE, nrow = 2)
 
-# Sanity Check: Row sums are 1.
-stopifnot(all(rowSums(dispersalMatrix) == 1))
+stopifnot(all(dispersalMatrix >= 0))
 
-dispersalMatrixConverted <- dispersalMatrix
-diag(dispersalMatrixConverted) <- 0
-dispersalMatrixConverted <- -dispersalMatrixConverted
+diag(dispersalMatrix) <- -Matrix::colSums(dispersalMatrix)
+
+# Sanity Check: Row sums are 0.
+stopifnot(all(Matrix::colSums(dispersalMatrix) == 0))
 
 dynSys <- function(t, y, parms) {
   with(as.list(parms), {
-    # indexes i, j, patches k, l.
-    # dy_(i,k)/dt = reproduction     : y_(i,k) * r
-    #             + interaction      : Sum_j a_ji y_(j,k) y_(i,k)
-    #             + dispersal gain   : Sum_{l != k} d_lk y_i
-    #             + dispersal loss   :
-    list(y * (r + a %*% y))
+    list(as.numeric(y * (r + a %*% y) + d %*% y))
+    # as.numeric since the solver doesn't know what Matrix::Matrices are.
   })
 }
 
 parmesan <- list(
-  r = redPool$ReproductionRate,
-  a = redComMat
+  r = rep(redPool$ReproductionRate, length(islands)),
+  a = Matrix::bdiag(rep(list(redComMat), length(islands))),
+  d = dispersalMatrix,
+  epsilon = Tolerance
 )
 
-# Repeat to match number of entries we are working with.
-parmesan <- lapply(parmesan, rep, times = nrow(dispersalMatrix))
-
+# Technically, a bit of extra work being done here since we already copied the
+# populations per island. The result is somewhat more readable though.
 abundance_init <- unlist(lapply(
   seq_along(redComs),
   function(i, com, pop, numPops) {
@@ -146,14 +203,44 @@ abundance_init <- unlist(lapply(
     k <- 1
     retval <- rep(0, numPops)
     for (j in 1:numPops) {
-      if (j %in% com) {
-        retval[j] <- pop[k]
+      if (j %in% com[[i]]) {
+        retval[j] <- pop[[i]][k]
         k <- k + 1
       }
     }
     return(retval)
   },
   com = redComs,
-  pop = populations,
+  pop = redPops,
   numPops = length(redCom)
 ))
+
+abundance <- deSolve::ode(
+  abundance_init,
+  times = ts,
+  func = dynSys,
+  parms = parmesan,
+  events = list(func = function(t, y, parms) {
+    y[y < parms$epsilon] <- 0
+    y
+  }, time = ts)
+)
+
+abundance_island <- lapply(
+  1:length(islands), function(i, x) {
+    x[, c(1, (1:length(redCom)) + 1 + (i - 1) * length(redCom))]
+  },
+  x = abundance
+)
+
+LawMorton1996_PlotAbundance(abundance_island[[1]]) -> figure1
+LawMorton1996_PlotAbundance(abundance_island[[2]]) -> figure2
+
+
+print(figure1 + ggplot2::ggtitle(paste("Init:", toString(redComs[[islands[1]]]))))
+print(figure2 + ggplot2::ggtitle(paste("Init:", toString(redComs[[islands[2]]]))))
+
+print("Island 1 Species:")
+print(SpeciesPresent(abundance_island[[1]]))
+print("Island 2 Species:")
+print(SpeciesPresent(abundance_island[[2]]))
