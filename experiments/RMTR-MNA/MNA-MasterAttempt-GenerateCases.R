fdf6b995df2f619415a802a9991e0778c3930a5b
Brennen-Fagan <92510995+Brennen-Fagan@users.noreply.github.com>
2021-11-03 16:21:23 +0000

    Changes to simulation file to reflect new priorities.
    Nothing terribly surprising in here from the ongoing talks.
    It also should hopefully be self-evident in the comments what we are currently thinking.
    
    Signed-off-by: Brennen-Fagan <92510995+Brennen-Fagan@users.noreply.github.com>

--- experiments/RMTR-MNA/MNA-MasterAttempt-GenerateCases.R
+++ experiments/RMTR-MNA/MNA-MasterAttempt-GenerateCases.R
@@ -1,60 +1,146 @@
+# Abstract: ####################################################################
 # This file lays out some of the "what" simulations we are looking at doing,
 # as well as creating the csv we will reference to initialise each simulation.
 
+library(RMTRCode2)
+
+# Parameters: ##################################################################
+systemBase <- list()
+systemMods <- list()
+
+### Universal Parameters: ######################################################
 # So we are currently thinking 10 histories for each pool-environment combo and
 # 10 pool-environment combinations for each parameter set.
+systemBase$historiesPerSystem <- 10
+systemBase$systemsPerParamSet <- 10
 
+### Framework Parameters: ######################################################
+# (Effectively, the ecological dynamics parameters.)
 # We are interested in varying the trophic structure with one of the following.
 #   1.   No Change.
 #   2.   Another Basal Layer (Below).
 #   3.   Another Consumer Layer (Above).
 #   4.   Both 2. and 3.
-#   5.   Strictly Basal species.
+#   5.   No noise.
 
+# In order:
+# Interaction Strength, Preferred Prey Size, Willingness to Deviate,
+# Efficiency, Basal Equilibrium Biomass Density, and Impact of Noise.
+systemBase$LM1996ParamSet <- c(0.01, 10, 0.5, 0.2, 100, 0.1)
+# Log body sizes: Basal Min, Basal Max, Consumer Min, Consumer Max
+systemBase$LM1996BodySize <- c(-2, -1, -1, 0)
+
+systemMods <- list(
+  PoolBodySizes = matrix(c(
+     0, 0, 0, 0,
+    -1, 0, 0, 0,
+     0, 0, 0, 1,
+    -1, 0, 0, 1,
+     0, 0, 0, 0
+  ), ncol = 4),
+  InteractionsNoiseMultiplier = c(1, 1, 1, 1, 0)
+)
+
+### Neutral Dynamics Parameters: ###############################################
 # We are interested in varying the history structure with one of the following.
-#   I.   No Change.
-#   II.  1/10 Extinction Rate.
-#   III. 10 * Extinction Rate.
-#   IV.  No Extinctions.
-#   V.   1/10 Arrival Rate.
-#   VI.  10 * Arrival Rate.
+#   I.    No Change.
+#   II.   1/10 Extinction Rate.
+#   III.  10 * Extinction Rate.
+#   IV.   No Extinctions.
+#   V.    1/10 Arrival Rate.
+#   VI.   10 * Arrival Rate.
+#   VII.  1/10 Extinction Rate. 1/10 Arrival Rate.
+#   VIII. 1/10 Extinction Rate. 10 * Arrival Rate.
+#   IX.   10 * Extinction Rate. 1/10 Arrival Rate.
+#   X.    10 * Extinction Rate. 10 * Arrival Rate.
+# (These are modified from the characteristic rate of the largest perturbation.)
 
+systemMods$NeutralRateMultipliers <- matrix(c(
+  # Note reversed from above.
+   1,    1,
+   1,    0.1,
+   1,   10,
+   1,    0,
+   0.1,  1,
+  10,    1,
+   0.1,  0.1,
+  10,    0.1,
+   0.1, 10,
+  10,   10
+), ncol = 2)
+
+### Spatial Dynamics Parameters: ###############################################
 # For the spatial component, we are interested in the following structures.
-#   a.   No Spatial coupling, distance Inf. (Eq. to Ring/2-Ring, dist. Inf.)
-#   b.   Ring Spatial coupling, distance 1E9.
-#   c.   Ring Spatial coupling, distance 1E6.
-#   d.   Ring Spatial coupling, distance 1E3.
-#   e.   Ring Spatial coupling, distance 1E0.
-#   f.   2-Ring Spatial coupling, distance 1E9.
-#   g.   2-Ring Spatial coupling, distance 1E6.
-#   h.   2-Ring Spatial coupling, distance 1E3.
-#   i.   2-Ring Spatial coupling, distance 1E0.
+#   a.   Ring Spatial coupling, distance 1E9.
+#   b.   Ring Spatial coupling, distance 1E6.
+#   c.   Ring Spatial coupling, distance 1E3.
+#   d.   Ring Spatial coupling, distance 1E0.
 
-# Finally, we might also consider the Anthropocene impact.
-# For this, we will be varying parameters midway through the experiment.
-# We do not change the pool or environment; 1 - 5 stay the same.
-# The options that we consider then are:
-#   Any of I - VI to any of I - VI.
-#   Any of a - e to any of a - e.
-#   Any of a, f - i to any of a, f - i as well as any of a, f - i with the rings
-#   changed from 1 - 5, 6 - 10 to 3 - 7, 8 - 2.
+systemMods$SpaceDistanceMultiplier <- c(1E9, 1E6, 1E3, 1E0)
 
-# As can be seen, this is a lot of combinations, roughly:
-#   10 * 10 * 5 * (6 * 6 * 9 + 6 * 5 * 5 + 6 * 5 * 10) = 387,000 runs.
+# Cases: #######################################################################
+# Some of these are special cases and are reduced.
+#   A = (1, c(IV, VII, VIII, IX, X), c(a, b, c, d)) * 10 * 10          => 2000
+#   B = (5, c(I, IV), c(a, b, c, d)) * 10 * 10                         =>  800
+# The remainder are grouped together.
+#   C = (c(1, 2, 3, 4), c(I, II, III, V, VI), c(a, b, c, d)) * 10 * 10 => 8000
+# For a grand total of 10,800 runs.
+# As an initial estimate, we saw about 30 minutes per run previously
+# and it seems reasonable to get about 30 cores from Viking.
+#   10,800 * 0.5 / 30 = 180 hours = 7.5 days.
 
-# We can reduce this somewhat if we acknowledge that runs with no treatment
-# are equivalent between the three alternation cases above.
-#   10 * 10 * 5 * (6 * 5 * 9 + 6 * 5 * 4 + 6 * 5 * 9 + 6 * 9) = 357,000 runs.
+# We use indices to refer to rows in the above parameters.
+cases <- list()
 
-# There is the potential to reduce this further by considering the treatment
-# runs as stemming from the same base system. In this case, we would perform
-#   10 * 10 * 5 * 6 * 9 = 27,000 "half-runs".
-# Then each run would be finished with its original conditions as
-#   -> 27,000 "half-runs"
-# as well as being used with treatments:
-#   27,000 * 5 "half-runs" for rate variation
-#   10 * 10 * 5 * 6 * (1 * 8 + 4 * 4 + 4 * (4 + 4)) = 168,000 "half-runs"
-#     for space variations.
-# This results again in 357,000 as expected, but half-runs instead of full runs.
-# This is more coding intensive, but could save us a significant amount of time.
-# The question is merely of trade-offs.
+# Case A: ######################################################################
+#   Effectively a supplemental figure, but what happens when we combine the
+#   changes in rates or we see an extreme "no elimination" case.
+#   We don't anticipate that there would be anything *interesting* here, mind,
+#   but we do think they are necessary to have touched base with and verified
+#   that nothing interesting actually happens.
+#   (1, c(IV, VII, VIII, IX, X), c(a, b, c, d))
+
+cases$A <- expand.grid(
+  Framework = 1,
+  Neutral = c(4, 7:10),
+  Space = 1:4
+)
+
+# Case B: ######################################################################
+#   What happens when we have precise control of the system. No noise, no
+#   randomness, just means of distributions doing precisely what we asked.
+#   For reference, this also seems like a good "no elimination" case to be
+#   aware of so that we know how much the eliminations and noise together
+#   drive the ecological dynamics versus how much the framework does.
+#   (5, c(I, IV), c(a, b, c, d))
+
+cases$B <- expand.grid(
+  Framework = 5,
+  Neutral = c(1, 4),
+  Space = 1:4
+)
+
+# Case C: ######################################################################
+#   This case has all the things that we think are going to be useful and/or
+#   interesting from the perspective of the paper.
+#   Effectively, this is what happens if we allow our system to change through
+#   time (I hesitate to say evolve since I am not wanting to use that
+#   terminology) using different parameter values that explore the relationship
+#   between the different types of "space" in the system and differing variants
+#   of the pool.
+#   (To be clear, I do not expect the pool's shape to really change the output,
+#   but it would be very important to have that under control if something were
+#   to go wrong with it. I would much rather that we look at a different
+#   framework, but I agree we should clean this one out to some extent first.)
+#   (c(1, 2, 3, 4), c(I, II, III, V, VI), c(a, b, c, d))
+
+cases$C <- expand.grid(
+  Framework = 1:4,
+  Neutral = 1:5,
+  Space = 1:4
+)
+
+# Make sure you know the cases you are working with...
+stopifnot(sum(sapply(cases, nrow)) == 108)
+
+
