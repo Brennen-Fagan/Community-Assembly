3dcabcfb5eca520307830283736aa999c6979188
Brennen-Fagan <92510995+Brennen-Fagan@users.noreply.github.com>
2021-11-23 21:56:13 +0000

    Everything seems ready to go.

--- experiments/RMTR-MNA/MNA-MasterAttempt-GeneratePMEsParallel.R
+++ experiments/RMTR-MNA/MNA-MasterAttempt-GeneratePMEsParallel.R
@@ -87,10 +87,19 @@ theEnvironments <- lapply(theEnvironments, function(env) {
     # and systemBase$historiesPerSystem histories per system.
 
     # The environment seeds.
-    temp <- matrix(unlist(strsplit(
+    envrseeds <- matrix(unlist(strsplit(
       cases$EnvironmentSeeds, split = thisSeparator, fixed = TRUE
     )),
     nrow = systemBase$environsPerSystem,
+    # ncol = systemBase$systemsPerParamSet * length(params)
+    # fills by column = by system x by parameter set
+    )
+
+    # The environment seeds.
+    histseeds <- matrix(unlist(strsplit(
+      cases$HistorySeeds, split = thisSeparator, fixed = TRUE
+    )),
+    nrow = systemBase$historiesPerSystem,
     # ncol = systemBase$systemsPerParamSet * length(params)
     # fills by column = by system x by parameter set
     )
@@ -148,7 +157,7 @@ theEnvironments <- lapply(theEnvironments, function(env) {
     # systemBase$systemsPerParamSet systems
     # systemBase$environsPerSystem environments per system
     # with 1 matrix per environment
-    cases$EnvironmentSeeds <- apply(temp, MARGIN = 2, toString)
+    cases$EnvironmentSeeds <- apply(envrseeds, MARGIN = 2, toString)
   })
 
   return(env)
@@ -158,7 +167,7 @@ theEnvironments <- lapply(theEnvironments, function(env) {
 print("Generate Matrices.")
 print(Sys.time())
 print(system.time(
-  foreach::foreach(
+  matrices <- foreach::foreach(
     envIndex = iterators::iter(
       rep(1:length(theEnvironments),
           times = unlist(lapply(theEnvironments,
@@ -166,94 +175,119 @@ print(system.time(
       )
     ),
     crow = iterators::iter(
-      dplyr::bind_rows(lapply(theEnvironments, function(env) env$cases)),
+      dplyr::bind_rows(lapply(theEnvironments,
+                              function(env) env$cases)),
       by = "row"
     )
-  ) %do% { # Faster without the overhead.
-    print(envIndex)
-    with(theEnvironments[[envIndex]],
-         matrs[[crow$Parameters]][[crow$System]] <-
-           RMTRCode2::CreateEnvironmentInteractions(
-             Pool = pools[[crow$Parameters]][[crow$System]],
-             NumEnvironments = systemBase$environsPerSystem,
-             ComputeInteractionMatrix = RMTRCode2::LawMorton1996_CommunityMat,
-             Parameters = systemBase$LM1996ParamSet * c(
-               1, 1, 1, 1, 1,
-               systemMods$InteractionsNoiseMultiplier[
-                 crow$Framework
-               ]),
-             EnvironmentSeeds = as.numeric(
-               strsplit(crow$EnvironmentSeeds, split = ", ", fixed = TRUE)[[1]]
-             )
-           )
-    )
-  }))
+  ) %dopar% {
+    # print(envIndex)
+    env <- theEnvironments[[envIndex]]
 
-# # Generate Events: ############################################################
-# theEnvironments <- lapply(
-#   theEnvironments, function(env) {
-#     with(env, {
-#       # evnts will be a row-associated list of pool-associated lists of
-#       # seed-associated event data frames.
-#       evnts <- lapply(
-#         1:nrow(cases), function(row, b, m, cs, p) {
-#           crow <- cases[row, ]
-#           poolGrp <- pools[[row]]
-#           matrGrp <- matrs[[row]]
-#
-#           # Using inheritance from global for this sep., sysBase, sysMods.
-#           seeds <- strsplit(crow$HistorySeeds,
-#                             split = thisSeparator,
-#                             fixed = TRUE)[[1]]
-#
-#           # Group using number of environments per system.
-#           seedGrps <- lapply(
-#             1:b$systemsPerParamSet, function(i, s) {
-#               s[1:b$historiesPerSystem
-#                 + (i - 1) * b$historiesPerSystem]
-#             }, s = seeds)
-#
-#           # We need to identify the appropriate rate to couple the events to.
-#           rateGrp <- lapply(
-#             matrGrp,
-#             function(Mat) {
-#               # Maximum magnitude eigenvalue should correspond to the (timescale
-#               # of the) fluctuations caused by the dominant eigenvector.
-#               max(unlist(lapply(
-#                 Mat$Mats, function(m) abs(eigen(m, only.values = TRUE)$values)
-#               )))
-#             }
-#           )
-#
-#
-#           lapply(
-#             seq_along(seedGrps), function(i, s, pl, r) {
-#               nrowpl <- nrow(pl[[i]])
-#               CreateAssemblySequence(
-#                 Species = nrowpl,
-#                 NumEnvironments = b$environsPerSystem,
-#                 ArrivalEvents = b$eventNumberFunc(
-#                   b$environsPerSystem, spec = nrowpl, const = 7
-#                   # More than 5 since we are not seeing enough mixing.
-#                 ),
-#                 ArrivalRate = r[[i]],
-#                 ArrivalFUN = ArrivalFUN_Example,
-#                 ExtinctEvents = b$eventNumberFunc(
-#                   b$environsPerSystem, spec = nrowpl, const = 7
-#                   # More than 5 since we are not seeing enough mixing.
-#                 ),
-#                 ExtinctRate = r[[i]],
-#                 ExtinctFUN = ExtinctFUN_Example,
-#                 HistorySeed = s[[i]]
-#               )
-#             }, s = seedGrps, pl = poolGrp, r = rateGrp)
-#         }, cs = cases, b = systemBase, m = systemMods, p = pools
-#       )
-#     })
-#
-#     return(env)
-#   }
-# )
+    templist <- list()
+
+    # env$matrs[[crow$Parameters]][[crow$System]] <-
+    templist[[paste(envIndex, crow$Parameters, crow$System)]] <-
+      RMTRCode2::CreateEnvironmentInteractions(
+        Pool = env$pools[[crow$Parameters]][[crow$System]],
+        NumEnvironments = systemBase$environsPerSystem,
+        ComputeInteractionMatrix = RMTRCode2::LawMorton1996_CommunityMat,
+        Parameters = systemBase$LM1996ParamSet * c(
+          1, 1, 1, 1, 1,
+          systemMods$InteractionsNoiseMultiplier[
+            crow$Framework
+          ]),
+        EnvironmentSeeds = as.numeric(
+          strsplit(crow$EnvironmentSeeds, split = ", ", fixed = TRUE)[[1]]
+        )
+      )
+
+    return(templist)
+  }))
+matrices <- matrices[order(unlist(lapply(matrices, names)))]
+
+for (mat in matrices) {
+  indices <- as.numeric(strsplit(names(mat), split = " ", fixed = TRUE)[[1]])
+  theEnvironments[[indices[1]]]$matrs[[indices[2]]][[indices[3]]] <- mat[[1]]
+}
+
+# Generate Events: ############################################################
+### Rearrange: ################################################################
+print("Rearrange for events. ")
+theEnvironments <- lapply(theEnvironments, function(env) {
+
+  with(env, {
+    # systemBase$systemsPerParamSet systems
+    # systemBase$environsPerSystem environments per system
+    # with 1 matrix per environment
+    cases$HistorySeeds <- apply(histseeds, MARGIN = 2, toString)
+  })
+
+  return(env)
+})
+
+### Calculate: ################################################################
+print("Generate Events.")
+print(Sys.time())
+print(system.time(
+  events <- foreach::foreach(
+    envIndex = iterators::iter(
+      rep(1:length(theEnvironments),
+          times = unlist(lapply(theEnvironments,
+                                function(env) {nrow(env$cases)}))
+      )
+    ),
+    crow = iterators::iter(
+      dplyr::bind_rows(lapply(theEnvironments,
+                              function(env) env$cases)),
+      by = "row"
+    )
+  ) %dopar% {
+    # print(envIndex)
+    env <- theEnvironments[[envIndex]]
+
+    # We need to identify the appropriate rate to couple the events to.
+    rate <-
+        # Maximum magnitude eigenvalue should correspond to the (timescale
+        # of the) fluctuations caused by the dominant eigenvector.
+        max(unlist(lapply(
+          env$matrs[[crow$Parameters]][[crow$System]]$Mats,
+          function(m) abs(eigen(m, only.values = TRUE)$values)
+        )))
+
+    nrowpl <- nrow(env$pools[[crow$Parameters]][[crow$System]])
+
+    templist <- list()
+
+    # env$matrs[[crow$Parameters]][[crow$System]] <-
+    templist[[paste(envIndex, crow$Parameters, crow$System)]] <-
+      RMTRCode2::CreateAssemblySequence(
+        Species = nrowpl,
+        NumEnvironments = systemBase$environsPerSystem,
+        ArrivalEvents = systemBase$eventNumberFunc(
+          systemBase$environsPerSystem, Spec = nrowpl, Const = 7
+          # More than 5 since we are not seeing enough mixing.
+        ),
+        ArrivalRate = rate,
+        ArrivalFUN = RMTRCode2::ArrivalFUN_Example,
+        ExtinctEvents = systemBase$eventNumberFunc(
+          systemBase$environsPerSystem, Spec = nrowpl, Const = 7
+          # More than 5 since we are not seeing enough mixing.
+        ),
+        ExtinctRate = rate,
+        ExtinctFUN = RMTRCode2::ExtinctFUN_Example,
+        HistorySeed = as.numeric(
+          strsplit(crow$HistorySeeds, split = ", ", fixed = TRUE)[[1]]
+        )
+      )
+
+    return(templist)
+  }))
+events <- events[order(unlist(lapply(events, names)))]
+
+for (evt in events) {
+  indices <- as.numeric(strsplit(names(evt), split = " ", fixed = TRUE)[[1]])
+  theEnvironments[[indices[1]]]$evnts[[indices[2]]][[indices[3]]] <- evt[[1]]
+}
 
 # Cleanup: ####################################################################
 parallel::stopCluster(clust)
