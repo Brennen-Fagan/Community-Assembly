a88e34b04e25d81a86a3641deddefb335005db0b
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2023-03-09 10:15:10 +0000

    Heterogeneity Decisions
    So a couple of decisions have been made. Figure 3 is uninteresting in the case of increased heterogeneity of environments or homogeneous environments. I'm committing this, and then moving it to extraneous in recognition.
    
    Second, it's been decided that the high heterogeneity system should be included as a separate column in figure 4 (and thus should contribute to the supplemental calculations, including supplemental figures and percentage of simulations completed). This will require some edits of the HighK6 to reflect this, but they will be committed here first.

---
+++ experiments/Figure4-MetasimulationStudy/Hik6/MNA-Highk6-GenerateCases.R
@@ -0,0 +1,209 @@
+# Abstract: ####################################################################
+# This file lays out some of the "what" simulations we are looking at doing,
+# as well as creating the csv we will reference to initialise each simulation.
+# For this version, the environmental coefficient of variation k6 is 0.2.
+
+library(RMTRCode2)
+
+# https://stackoverflow.com/a/15373917
+thisFile <- function() {
+  cmdArgs <- commandArgs(trailingOnly = FALSE)
+  needle <- "--file="
+  match <- grep(needle, cmdArgs)
+  if (length(match) > 0) {
+    # Rscript
+    return(normalizePath(sub(needle, "", cmdArgs[match])))
+  } else {
+    # 'source'd via R console
+    return(normalizePath(sys.frames()[[1]]$ofile))
+  }
+}
+
+# Initialisation: ##############################################################
+thisDirectory <- dirname(thisFile())
+thisFilePrefix <- "MNA-Hik6"
+thisFileSuffix <- "-Cases.csv"
+thisSeparator <- "|"
+
+# > runif(1) * 1E8
+# [1] 24223547
+if (exists(".Random.seed")) {
+  old.seed <- .Random.seed
+}
+set.seed(24223547)
+
+# Parameters: ##################################################################
+systemBase <- list()
+systemMods <- list()
+
+### Universal Parameters: ######################################################
+# So we are currently thinking 10 histories for each pool-environment combo and
+# 10 pool-environment combinations for each parameter set.
+systemBase$historiesPerSystem <- 10
+systemBase$systemsPerParamSet <- 10
+systemBase$environsPerSystem <- 10
+
+# Threshold below which species are removed during calculations.
+systemBase$eliminationThreshold <- 10^-4
+# Distance above a threshold that a new immigration community starts at.
+# Traill et al. 2007 used for inspiration.
+systemBase$arrivalDensity <- systemBase$eliminationThreshold * 4 * 10 ^ 3
+
+# Maximum time solver can proceed without checking for elimination.
+systemBase$maximumTimeStep <- 1
+# Minimum number of steps to reach next event to smooth.
+systemBase$betweenEventSteps <- 30
+
+systemBase$species <- c(Basal = 34, Consumer = 66)
+systemBase$speciesSpeeds <- 1
+
+# See the coupon collector's problem.
+systemBase$eventNumberFunc <- function(Envs, Spec, Const = 5) {
+  Envs * ceiling(Spec * (log(Spec + Const)))
+}
+
+### Framework Parameters: ######################################################
+# (Effectively, the ecological dynamics parameters.)
+# We are interested in varying the trophic structure with one of the following.
+#   1.   No Change.
+#   2.   Another Basal Layer (Below).
+#   3.   Another Consumer Layer (Above).
+#   4.   Both 2. and 3.
+#   5.   No noise.
+
+# In order:
+# Interaction Strength, Preferred Prey Size, Willingness to Deviate,
+# Efficiency, Basal Equilibrium Biomass Density, and Impact of Noise.
+systemBase$LM1996ParamSet <- c(0.01, 10, 0.5, 0.2, 100, 0.1)
+# Log body sizes: Basal Min, Basal Max, Consumer Min, Consumer Max
+systemBase$LM1996BodySize <- c(-2, -1, -1, 0)
+
+systemMods <- list(
+  PoolBodySizes = matrix(c(
+     0, 0, 0, 0,
+    -1, 0, 0, 0,
+     0, 0, 0, 1,
+    -1, 0, 0, 1,
+     0, 0, 0, 0,
+     0, 0, 0, 0
+  ), ncol = 4, byrow = TRUE),
+  InteractionsNoiseMultiplier = c(1, 1, 1, 1, 0, 2)
+)
+
+### Neutral Dynamics Parameters: ###############################################
+# We are interested in varying the history structure with one of the following.
+#   I.    No Change.
+#   II.   1/10 Extinction Rate.
+#   III.  10 * Extinction Rate.
+#   IV.   No Extinctions.
+#   V.    1/10 Arrival Rate.
+#   VI.   10 * Arrival Rate.
+#   VII.  1/10 Extinction Rate. 1/10 Arrival Rate.
+#   VIII. 1/10 Extinction Rate. 10 * Arrival Rate.
+#   IX.   10 * Extinction Rate. 1/10 Arrival Rate.
+#   X.    10 * Extinction Rate. 10 * Arrival Rate.
+# (These are modified from the characteristic rate of the largest perturbation.)
+
+systemMods$NeutralRateMultipliers <- matrix(c(
+  # Note reversed from above.
+   1,    1,
+   1,    0.1,
+   1,   10,
+   1,    0,
+   0.1,  1,
+  10,    1,
+   0.1,  0.1,
+  10,    0.1,
+   0.1, 10,
+  10,   10
+), ncol = 2, byrow = TRUE)
+
+### Spatial Dynamics Parameters: ###############################################
+# For the spatial component, we are interested in the following structures.
+#   x.   Ring Spatial coupling, distance Inf. (Equivalent to None)
+#   a.   Ring Spatial coupling, distance 1E9.
+#   b.   Ring Spatial coupling, distance 1E8.
+#   c.   Ring Spatial coupling, distance 1E7.
+#   d.   Ring Spatial coupling, distance 1E6.
+#   e.   Ring Spatial coupling, distance 1E5.
+#   f.   Ring Spatial coupling, distance 1E4.
+#   g.   Ring Spatial coupling, distance 1E3.
+#   h.   Ring Spatial coupling, distance 1E2.
+#   i.   Ring Spatial coupling, distance 1E1.
+#   j.   Ring Spatial coupling, distance 1E0.
+
+systemMods$SpaceDistanceMultiplier <- 10^c(Inf, 9:0)
+
+# Cases: #######################################################################
+# Some of these are special cases and are reduced.
+#   A = (1, c(IV, VII, VIII, IX, X), c(x, a:h)) * 10 * 10          =>  4500
+#   B = (5, c(I, IV), c(x, a:h)) * 10 * 10                         =>  1800
+# The remainder are grouped together.
+#   C = (c(1, 2, 3, 4), c(I, II, III, V, VI), c(x, a:h)) * 10 * 10 => 18000
+# For a grand total of 21,600 runs.
+# As an initial estimate, we saw about 30 minutes per run previously
+# and it seems reasonable to get about 30 cores from Viking.
+#   24,300 * 0.5 / 30 = 360 hours = 30 days.
+
+# We use indices to refer to rows in the above parameters.
+cases <- list()
+
+### Case A: ####################################################################
+#   We are aiming to fill in the high dispersal cases that we initially missed.
+#   These cases are useful for telling the story that we want to tell.
+#   (1, c(IV, VII, VIII, IX, X), c(a, b, c, d))
+
+cases$A <- expand.grid(
+  Framework = 6,
+  Neutral = 1,
+  Space = 1:11
+)
+
+
+# Make sure you know the cases you are working with...
+stopifnot(sum(sapply(cases, nrow)) == 11)
+
+# Preparations: ################################################################
+### Assign Random Seeds: #######################################################
+# Assign to each case historiesPerSystem * systemsPerParamSet history seeds,
+# and systemsPerParamSet sets of enviromentsPerPool environment seeds.
+cases <- lapply(
+  cases,
+  function(case, base) {
+    case$PoolSeeds <- replicate(nrow(case), paste0(runif(
+      base$systemsPerParamSet) * 1E8,
+      collapse = thisSeparator))
+    case$HistorySeeds <- replicate(nrow(case), paste0(runif(
+      base$historiesPerSystem * base$systemsPerParamSet) * 1E8,
+      collapse = thisSeparator))
+    case$EnvironmentSeeds <- replicate(nrow(case), paste0(runif(
+      base$environsPerSystem * base$systemsPerParamSet) * 1E8,
+      collapse = thisSeparator))
+    case
+  }, base = systemBase
+  )
+
+# Creation: ####################################################################
+### Cases: #####################################################################
+lapply(
+  seq_along(cases),
+  function(i, case, name) {
+    write.table(
+      cases[[i]], file = file.path(
+        thisDirectory,
+        paste0(thisFilePrefix, name[i], thisFileSuffix)
+      ), sep = ","
+    )
+  }, case = cases,  name = names(cases)
+)
+
+### Parameters: ################################################################
+save(systemBase, systemMods, thisSeparator, file = file.path(
+  thisDirectory,
+  paste0(thisFilePrefix, "Parameters", ".RData")
+))
+
+# Cleanup: #####################################################################
+if (exists("old.seed")) {
+  set.seed(old.seed)
+}
---
+++ experiments/Figure4-MetasimulationStudy/Hik6/MNA-Highk6-VikingRunHalves.R
@@ -0,0 +1,246 @@
+# For running on Viking in an embarrassingly parallel manner.
+# We have the cases, pools, matr(ice)s, and ev(e)nts already generated.
+# We now need
+#   to receive the selection to use,
+#   to load the appropriate system,
+#   to generate the master interaction matrix,
+#   to generate the per capita dynamics,
+#   to generate the spatial dynamics,
+#   to run the system, and
+#   finally to save the system.
+
+# Furthermore, there is a danger of a segfault during evaluation.
+# To avoid this, we'll be doing two things.
+# First, run the code in halves. We'll run 1-1, 1-2, 2-1, 2-2, 3-1, 3-2, etc.
+# Second, we'll be saving after each half in order to be on the safe side.
+# We can then skip halves that have already been computed.
+
+# Universal Settings: #########################################################
+EliminationThreshold <- 10^-4 # Below which species are removed from internals
+ArrivalDensity <- EliminationThreshold * 4 * 10 ^ 3 # Traill et al. 2007
+
+MaximumTimeStep <- 1 # Maximum time solver can proceed without elimination.
+BetweenEventSteps <- 30 # Number of steps to reach next event to smooth.
+
+directory <- '.'
+
+outputLocation <- file.path(directory, paste0("SaveOutput_", Sys.Date()))
+if (!dir.exists(outputLocation)) {
+  dir.create(outputLocation, showWarnings = FALSE)
+}
+
+# Set-up: #####################################################################
+print("Loading libraries")
+librarypath <- file.path(directory, "Rlibs")
+if (!dir.exists(librarypath)) {
+  dir.create(librarypath, showWarnings = FALSE)
+}
+.libPaths(c(librarypath, .libPaths()))
+
+allLibraryPaths <- .libPaths()
+
+packages <- c(
+  "dplyr",
+  "Matrix"
+)
+
+# Does not work because it tries to use the system-wide libraries... Oops.
+# update.packages(repos = 'https://cloud.r-project.org',
+#                 oldPkgs = packages, ask = FALSE)
+
+for (package in packages) {
+  if (!require(package, character.only = TRUE)) {
+    install.packages(package, lib = librarypath,
+                     repos = 'https://cloud.r-project.org',
+                     dependencies = TRUE)
+  }
+  library(package, character.only = TRUE)
+}
+
+if (!require("RMTRCode2", character.only = TRUE)) {
+  install.packages(
+    "RMTRCode2_0.2.tar.gz", lib = librarypath,
+    repos = NULL, type = "source"
+  )
+}
+library(RMTRCode2)#, lib.loc = librarypath) # lib.loc shouldn't be necessary.
+
+# Receive the Selection: ######################################################
+print("Receive")
+# Two CArgs:
+#   Case:       A: 1,
+#   Row Number: A: 1000, (i.e. `nrow`s)
+cargs <- as.numeric(commandArgs(trailingOnly = TRUE))
+print(cargs)
+
+# Load the System: ############################################################
+print("Load")
+cases <- NULL
+pools <- NULL
+matrs <- NULL
+evnts <- NULL
+
+candidates <- dir(pattern = "MNA[-]Hik6.+[-]Cases[-]Prepared[.]RData$",
+                  path = "Prepared_2023-03-03",
+                  full.names = TRUE)
+
+stopifnot(length(candidates) == 1)
+
+loaded <- load(candidates[cargs[1]])
+outputName <- strsplit(basename(candidates[cargs[1]]),
+                       split = ".", fixed = TRUE)[[1]]
+outputName <- paste0(outputName[-length(outputName)],
+                     "-", cargs[1], "-", cargs[2], "-")#, ".RData")
+
+stopifnot(
+  c("cases", "pools", "matrs", "evnts") %in% loaded,
+  unlist(lapply(list(cases, pools, matrs, evnts),
+                function(i) !is.null(i)))
+)
+
+case <- cases[cargs[2], ]
+pool <- pools[[case$Parameters]][[case$System]]
+matr <- matrs[[case$Parameters]][[case$System]]
+evnt <- evnts[[case$Parameters]][[case$System]]
+
+Environments <- length(matr$Mats)
+
+# Generate the master interaction matrix: #####################################
+print("Master")
+IntMats <- Matrix::bdiag(matr$Mats)
+
+# Generate the per capita dynamics: ###########################################
+print("PerCap")
+PerCapitaDynamics <- RMTRCode2::PerCapitaDynamics_Type1(
+  pool$ReproductionRate, IntMats,
+  NumEnvironments = Environments
+)
+
+# Generate the spatial dynamics: ##############################################
+print("Space")
+PerIslandDistance <- switch(
+  case$Space,
+  Inf, 1e9, 1e8, 1e7, 1e6,
+  1e5, 1e4, 1e3, 1e2, 1e1, 1e0
+)
+
+# Ring Dynamics
+DistanceMatrix <- Matrix::bandSparse(
+  Environments, k = c(-1, 1),
+  diagonals = list(rep(PerIslandDistance, Environments - 1),
+                   rep(PerIslandDistance, Environments - 1))
+)
+DistanceMatrix[Environments, 1] <- PerIslandDistance
+DistanceMatrix[1, Environments] <- PerIslandDistance
+
+DispersalMatrices <- RMTRCode2::CreateDispersalMatrix(
+  EnvironmentDistances = DistanceMatrix,
+  SpeciesSpeeds = rep(1, nrow(pool))
+)
+
+# Run the system: #############################################################
+print("Run")
+# Run times appear to be short enough that we can just run each set of histories
+# (per pool-environ combination) on the same node without hitting time-out.
+#results <- list()
+folderCandidates <- dir(pattern = "SaveOutput[_][0-9]{4}[-][0-9]{2}[-][0-9]{2}")
+fileCandidates_full <- dir(folderCandidates, full.names = TRUE)
+fileCandidates <- basename(fileCandidates_full)
+for (i in seq_along(evnt)){
+  print(i)
+  fileName1 <- file.path(
+    outputLocation, paste0(
+      outputName, i, "-", 1, ".RData"
+    )
+  )
+
+  ev <- evnt[[i]]
+  stopTime <- max(ev$Events$Times) / 2
+
+  if (
+    #!file.exists(fileName1)
+    !any(indices <- (paste0(
+      outputName, i, "-", 1, ".RData"
+    ) == fileCandidates))
+    ) {
+    print(1)
+    ev$Events <- ev$Events %>% dplyr::filter(Times < stopTime)
+    results <- RMTRCode2::MultipleNumericalAssembly_Dispersal(
+      Pool = pool, NumEnvironments = Environments,
+      InteractionMatrices = matr,
+      Events = ev,
+      PerCapitaDynamics = PerCapitaDynamics,
+      DispersalMatrix = DispersalMatrices,
+      EliminationThreshold = EliminationThreshold,
+      ArrivalDensity = ArrivalDensity,
+      MaximumTimeStep = MaximumTimeStep,
+      BetweenEventSteps = BetweenEventSteps,
+      Verbose = FALSE
+    )
+
+    print("Save")
+
+    save(
+      results, file = fileName1
+    )
+  }
+
+  fileName2 <- file.path(
+    outputLocation, paste0(
+      outputName, i, "-", 2, ".RData"
+    )
+  )
+
+  if (
+    #!file.exists(fileName2)
+    !any((paste0(
+      outputName, i, "-", 2, ".RData"
+    ) == fileCandidates))
+    ) {
+
+    if(!exists("results")) {
+      load(tail(fileCandidates_full[indices], n = 1))
+    }
+
+    print(2)
+
+    ev <- evnt[[i]]
+    # We'll stop immediately before the last event, then make sure to include
+    # it as the starting point for the next event.
+    lastEventTime <- max(results$Events$Times)
+    ev$Events <- ev$Events %>% dplyr::filter(Times >= lastEventTime)
+    stopRow <- which.max(results$Abundance[, 1] >= lastEventTime) - 1
+
+    results <- RMTRCode2::MultipleNumericalAssembly_Dispersal(
+      PopulationInitial = results$Abundance[stopRow, -1],
+      TimeInitial = results$Abundance[stopRow, 1],
+      Pool = pool, NumEnvironments = Environments,
+      InteractionMatrices = matr,
+      Events = ev,
+      PerCapitaDynamics = PerCapitaDynamics,
+      DispersalMatrix = DispersalMatrices,
+      EliminationThreshold = EliminationThreshold,
+      ArrivalDensity = ArrivalDensity,
+      MaximumTimeStep = MaximumTimeStep,
+      BetweenEventSteps = BetweenEventSteps,
+      Verbose = FALSE
+    )
+
+    print("Save")
+
+    save(
+      results, file = fileName2
+    )
+  }
+
+  rm(results)
+}
+
+
+# Save the system: ############################################################
+# print("Save")
+# save(
+#   results, file = file.path(
+#     outputLocation, outputName
+#   )
+# )
