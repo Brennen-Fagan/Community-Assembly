3ff8b79bae8dbd0b611a613955e0bdbcbacf3248
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2023-07-20 10:45:43 +0100

    Update Combined-ExampleOutcome-Image.R
    Iterated Figure 3 like plots. Note that RStudio has been locking up on attempts to run ggsave, so I've been doing it manually.
    
    (Forgot to commit earlier.)

--- experiments/Mutualism/Combined-ExampleOutcome-Image.R
+++ experiments/Mutualism/Combined-ExampleOutcome-Image.R
@@ -10,6 +10,7 @@ library(dplyr)        # Data Manipulation
 library(tidyr)        # Data Pivotting
 library(ggplot2)      # 2-D Plot
 library(gridExtra)
+library(patchwork)
 
 # Problems with X11
 options(bitmapType = "cairo")
@@ -22,7 +23,8 @@ burn_in <-0 # 1E1 # time units
 
 # All .RData
 files_dat <- dir(
-  path = "Data_2023-06-26", # "Data_2022-09-16",
+  path = c("Data_2023-06-26", "Data_2023-06-27", "Data_2023-07-06",
+           "Data2_2023-06-30"), # "Data_2022-09-16",
   pattern = ".+Example.+[.]RData$", # "MNA[-]ExampleOutcome[-].+[.]RData$",
   full.names = TRUE
 )
@@ -252,8 +254,132 @@ DiversityRibbons_Gamma <- Diversity %>% dplyr::filter(
 # Plots: #######################################################################
 
 pasteCustom <- function(x, y) {
-  paste0("(", x, ", ", y, ")")
+  factor(x, levels = c(
+    "0", "2e-09", "2e-08", "2e-07", "2e-06", "2e-05",
+    "0.0002", "0.001998", "0.0198", "0.1813", "0.8647"
+  ), ordered = TRUE)
+  # paste0("(", x, ", ", y, ")")
   # ifelse(y == max(y), "None",
   #        ifelse(y == min(y), "Full", "Med."))
 }
 legend_bl_name <- "Dispersal"
+
+Diversity <- Diversity %>% dplyr::mutate(
+  Measurement2 = dplyr::case_when(
+    Measurement == "Jaccard" ~ "Spatial Diss.",
+    Measurement == "Richness" & Environment == "Gamma" ~ "Regional Rich.",
+    Measurement == "Richness" & Environment == "Mean" ~ "Local Rich.", # Panel
+    Measurement == "Richness"  ~ "Local Rich.", # Otherwise
+    TRUE ~ Measurement
+  )
+)
+
+PLOT_Presence <- lapply(
+  SpeciesPresence$SimType %>% unique(),
+  function(SType) {
+    ggplot2::ggplot(
+      SpeciesPresence %>% dplyr::filter(SimType == SType, Dispersal != 1),
+      ggplot2::aes(x = Time, y = RepRateID, color = Count)
+    ) + ggplot2::geom_point(
+      shape = '.'
+    ) + ggplot2::scale_color_viridis_c(
+      direction = -1,
+      limits = c(1, 10)
+    ) + ggplot2::facet_grid(
+      SimType ~ #Space +
+        factor(Dispersal, levels = c(
+          "0", "2e-09", "2e-08", "2e-07", "2e-06", "2e-05",
+          "0.0002", "0.001998", "0.0198", "0.1813", "0.8647"
+        ), ordered = TRUE),
+      scales = "free_y"
+    ) + ggplot2::geom_hline(
+      yintercept = 1/3 * nrow(PoolsMats[[1]]$Pool), color = "red"
+    ) + ggplot2::labs(
+      y = "Species by Size",
+      x = paste0("Time, ", divide_time_by, " units"),
+      tag = "a)"
+    ) + ggplot2::theme_bw(
+    ) + ggplot2::theme(
+      #axis.text.x = ggplot2::element_blank(),
+      plot.tag.position = c(0.02, 0.98)
+    )
+  }
+)
+
+# ggplot2::ggsave(
+#   patchwork::wrap_plots(PLOT_Presence, nrow = length(PLOT_Presence), ncol = 1),
+#   "PresentPlots.png", units = "in", width = 12, height = 9
+# )
+
+PLOT_B <- ggplot2::ggplot(
+  Diversity %>% dplyr::filter(
+    Measurement2 %in% c("Spatial Diss.", "Local Rich.", "Regional Rich."),
+    Environment != "Mean"
+  ),
+  ggplot2::aes(
+    x = Time,
+    y = Value,
+    color = pasteCustom(Dispersal, Space)
+  )
+) + ggplot2::geom_line(
+  # alpha = 0.4,
+  mapping = ggplot2::aes(
+    group = interaction(Dispersal, Environment),
+    alpha = ifelse(Measurement2 == "Regional Rich.", 1, 0.4)
+  )
+) + ggplot2::geom_line(
+  data = Diversity %>% dplyr::filter(
+    Measurement2 %in% c("Spatial Diss.", "Local Rich.", "Regional Rich."),
+    Environment == "Mean"
+  ),
+  size = 1.5
+) + ggplot2::geom_ribbon(
+  data = dplyr::bind_rows(
+    DiversityRibbons,
+    DiversityRibbons_Gamma
+  ) %>% dplyr::mutate(
+    Measurement2 = dplyr::case_when(
+      Measurement == "Jaccard" ~ "Spatial Diss.",
+      Measurement == "Richness" ~ "Local Rich.",
+      TRUE ~ Measurement
+    )
+  ),
+  mapping = ggplot2::aes(
+    ymin = Low,
+    ymax = High,
+    x = Time,
+    fill = pasteCustom(Dispersal, Space)
+  ),
+  alpha = 0.4,
+  inherit.aes = FALSE
+) + ggplot2::theme_bw(
+) + ggplot2::labs(
+  y = "Value", # Number of Species",
+  x = paste0("Time, ", divide_time_by, " units"),
+  tag = "b)"
+  # x = ""
+) + ggplot2::theme(
+  plot.tag.position = c(0.02, 0.98),
+  strip.text.x = ggplot2::element_text(size = 8)
+# ) + ggplot2::scale_color_manual(
+#   name = legend_bl_name,
+#   values = c("darkorange", "plum1", "cyan")
+# ) + ggplot2::scale_fill_manual(
+#   name = legend_bl_name,
+#   values = c("darkorange4", "plum4", "cyan4")
+) + ggplot2::facet_wrap(
+  SimType ~ factor(
+    Measurement2, ordered = T,
+    levels = c("Local Rich.", "Regional Rich.", "Spatial Diss.")
+  ), #nrow = unique(Diversity$SimType),
+  ncol = 3,
+  scales = "free"
+) + ggplot2::scale_alpha(guide = "none") + ggplot2::coord_cartesian(
+  ylim = c(0, NA)
+)
+
+
+# ggplot2::ggsave(
+#     PLOT_B,
+#   "DiversityPlots.png", units = "in", width = 12, height = 9
+# )
