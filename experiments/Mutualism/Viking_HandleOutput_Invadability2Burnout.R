034544479443449452404e95248768947f59ccf6
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2023-07-21 10:42:25 +0100

    Update Viking_HandleOutput_Invadability2Burnout.R
    Getting the sense that this might not be the most up-to-date version.

--- experiments/Mutualism/Viking_HandleOutput_Invadability2Burnout.R
+++ experiments/Mutualism/Viking_HandleOutput_Invadability2Burnout.R
@@ -16,14 +16,14 @@ if (!dir.exists(outputLocation)) {
 }
 
 # Libraries: ##################################################################
-print("Loading libraries")
-librarypath <- file.path(".", "Rlibs")
-if (!dir.exists(librarypath)) {
-  dir.create(librarypath, showWarnings = FALSE)
-}
-.libPaths(c(librarypath, .libPaths()))
-
-allLibraryPaths <- .libPaths()
+# print("Loading libraries")
+# librarypath <- file.path(".", "Rlibs")
+# if (!dir.exists(librarypath)) {
+#   dir.create(librarypath, showWarnings = FALSE)
+# }
+# .libPaths(c(librarypath, .libPaths()))
+#
+# allLibraryPaths <- .libPaths()
 
 packages <- c(
   "dplyr",      # Data Manipulation
@@ -40,39 +40,39 @@ packages <- c(
 #                 oldPkgs = packages, ask = FALSE)
 
 for (package in packages) {
-  if (!require(package, character.only = TRUE)) {
-    install.packages(package, lib = librarypath,
-                     repos = 'https://cloud.r-project.org',
-                     dependencies = TRUE)
-  }
+  # if (!require(package, character.only = TRUE)) {
+  #   install.packages(package, lib = librarypath,
+  #                    repos = 'https://cloud.r-project.org',
+  #                    dependencies = TRUE)
+  # }
   library(package, character.only = TRUE)
 }
 
-if (!require("RMTRCode2", character.only = TRUE)) {
-  install.packages(
-    "RMTRCode2_2022-02-12_1.0.tar.gz", lib = librarypath,
-    repos = NULL, type = "source"
-  )
-}
+# if (!require("RMTRCode2", character.only = TRUE)) {
+#   install.packages(
+#     "RMTRCode2_2022-02-12_1.0.tar.gz", lib = librarypath,
+#     repos = NULL, type = "source"
+#   )
+# }
 library(RMTRCode2)#, lib.loc = librarypath) # lib.loc shouldn't be necessary.
 
-if (!exists("CalculateLocalInvadables_KnockOn",
-            where = asNamespace("RMTRCode2"),
-            mode = "function")) {
-  source(file.path(".", "Invadability.R"))
-  if (!exists("CalculateLocalInvadables_KnockOn")) {
-    stop("Could not find invadability calculation.")
-  }
-}
+# if (!exists("CalculateLocalInvadables_KnockOn",
+#             where = asNamespace("RMTRCode2"),
+#             mode = "function")) {
+#   source(file.path(".", "Invadability.R"))
+#   if (!exists("CalculateLocalInvadables_KnockOn")) {
+#     stop("Could not find invadability calculation.")
+#   }
+# }
 
 # # Receive the Selection: ######################################################
 # print("Receiving number of cores:")
 #
-# # cargs <- 4
+cargs <- 4
 # cargs <- as.numeric(commandArgs(trailingOnly = TRUE))
 #
-# clust <- parallel::makeCluster(cargs[1], outfile = "")
-# doParallel::registerDoParallel(clust)
+clust <- parallel::makeCluster(cargs[1], outfile = "")
+doParallel::registerDoParallel(clust)
 #
 # # Send locally installed packages to cluster.
 # parallel::clusterExport(
@@ -87,94 +87,6 @@ if (!exists("CalculateLocalInvadables_KnockOn",
 #     library(Matrix)
 #   }
 # )
-
-# Functions: ##################################################################
-
-# Recycling from Viking_HandleOutput_Diversity.R.
-thinAndCalculateInvadabilities <- function(loaded, dyn, dis) {
-  # We can't handle all of the data that we are going to be looking at;
-  # a small sample had ~180k rows for ~5.3k events = ~34 rows per event.
-  # To reduce it, we will divide time up so that there are about the
-  # preferred number of rows per event.
-  bythin <- floor((nrow(loaded$Abundance)
-                   / nrow(loaded$Events))
-                  / preferred_rows_per_event)
-
-  loaded$Abundance <- loaded$Abundance[seq(from = 1,
-                                           to = nrow(loaded$Abundance),
-                                           by = bythin), ]
-
-  # Remove illegal values (that the numerical engine uses as inbetweens).
-  toEliminate <- loaded$Abundance[, -1] <
-    loaded$Parameters$EliminationThreshold & loaded$Abundance[, -1] > 0
-  loaded$Abundance[, -1][toEliminate] <- 0
-  loaded$Abundance[, 1] <- loaded$Abundance[, 1] / divide_time_by
-
-  # Identify and grab only the last pre-burn-out time:
-  target <- which.max(loaded$Abundance[, 1] > burnout / divide_time_by) - 1
-
-  if (target == 0) {
-    stop("Beginning of burn-out not found.")
-  }
-
-  # # Convert to Binary, since we are only interested in richness.
-  # loaded$Abundance[, -1] <- loaded$Abundance[, -1] > 0
-
-  # loaded$Abundance is now prepared.
-  # Now, we calculate richnesses.
-  print("prep")
-
-  ### Invadability: #####################################################
-  invadability <- CalculateLocalInvadables_KnockOn(
-    Abundance = loaded$Abundance[target, -1],
-    PerCapitaDynamics = dyn,
-    Environments = loaded$NumEnvironments,
-    ArrivalDensity = loaded$Parameters$ArrivalDensity,
-    DispersalMatrix = dis,
-    TimeScale = loaded$ReactionTime
-  )
-
-  binary <- rep(FALSE, length(loaded$Abundance[target, -1]))
-  binary[invadability$candidates] <- invadability$invadable
-
-  invadabilityMat <- (
-    Matrix::drop0(Matrix::Matrix(binary, nrow = 1, ncol = length(binary)))
-  )
-
-  # candidateRegional = candidates who are not present on any patch
-  # invadableRegional = above candidates, but who can invade any patch.
-  # Matrix(Invadability$Invadabilities[[1]]$invadability[,], nrow = 100, ncol = 10)
-  # Compare Matrix(Invadability$Invadabilities[[1]]$species, nrow = 100, ncol = 10)
-  # Then candidates are then the all 1 rows of
-  # Matrix(loaded$Abundance[target, -1] < EliminationThreshold, nrow = 100, ncol = 10)
-  # and they are invadable if there are any 1's in there corresponding row of
-  # Matrix(Invadability$Invadabilities[[1]]$invadability[,], nrow = 100, ncol = 10)
-
-  theSpecies <- 1:((ncol(loaded$Abundance) - 1) / loaded$NumEnvironments)
-  ### Return Invadabilities: ###############################################
-  return(list(
-    invadability = invadabilityMat, # Sparse
-    time = loaded$Abundance[target, 1], # Not Sparse.
-    species = rep(
-      theSpecies,
-      loaded$NumEnvironments
-    ), # i.e. 1 2 3 1 2 3 1 2 3, Not Sparse.
-    environment = rep(
-      1:loaded$NumEnvironments,
-      each = ((ncol(loaded$Abundance) - 1) / loaded$NumEnvironments)
-    ), # i.e. 1 1 1 2 2 2 3 3 3, Not Sparse.
-    speciesRegional = theSpecies,
-    invadabilityRegional = as.numeric(
-      apply(Matrix(loaded$Abundance[target, -1] <
-                     loaded$Parameters$EliminationThreshold,
-                   nrow = 100, ncol = 10), 1, all) *
-      apply(Matrix(invadabilityMat[,], nrow = 100, ncol = 10), 1, any)
-      ),
-    effectAbundance = invadability$effectAbundance,
-    effectRichness = invadability$effectRichness,
-    effectEstablish = invadability$effectEstablish
-  ))
-}
 
 # Files: ######################################################################
 print("Identifying files.")
