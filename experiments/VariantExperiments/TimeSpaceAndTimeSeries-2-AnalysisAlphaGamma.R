5c896e44cc6c6083b356d61b2f132cbd10d9be68
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2023-11-15 10:44:44 +0000

    Moving ConvertPreparedToBeta
    Noted a couple of problems (things I didn't implement) and fixed a few bugs (forgetting to add Method = method) during the move.

--- experiments/VariantExperiments/TimeSpaceAndTimeSeries-2-AnalysisAlphaGamma.R
+++ experiments/VariantExperiments/TimeSpaceAndTimeSeries-2-AnalysisAlphaGamma.R
@@ -234,72 +234,9 @@ bootstrapSamplesPairedBeta_source <- bootstrapSamples %>% addDistanceColumns(
 #    once for fixed patches and
 #    once for fixed times.
 
+# Note the ", " combination is reserved and required in ConvertPreparedToBeta.
 postfixes <- c(", All", ", Det. In Control", ", Not Det. In Control")
 betacolumns <- c("SamplingIDs", "SamplingIDsNative", "SamplingIDsInvasive")
-
-# # Prepared looks like, with indicator as the first grouping variable.
-# bootstrapSamplesPairedBeta_source %>% dplyr::filter(
-#   TimeGapNumber == preferredTimeGap
-# ) %>% dplyr::group_by(
-#   DistanceFromCenterExpRev, Bootstrap, Type
-# )
-# # or like
-# bootstrapSamplesPairedBeta_source %>% dplyr::filter(
-#   DistanceFromCenterExpRev == 0
-# ) %>% dplyr::group_by(
-#   TimeGapNumber, Bootstrap, Type
-# )
-
-ConvertPreparedToBeta <- function(
-  prepared, columns, method, presenceabsence, postfixes, indicator
-) {
-  # Generate the Beta Diversities Across the Columns
-  retval <- lapply(
-    columns,
-    function(Column, dat, method) {
-      temp <- dat %>% dplyr::group_modify(
-        .f = SpeciesStringsToBeta,
-        SpeciesColumn = Column,
-        AbundanceColumn = Column,
-        PresenceAbsence = presenceabsence
-      )
-      colnames(temp)[colnames(temp) == "Beta"] <- method
-      return(temp)
-    },
-    dat = prepared,
-    method = method
-  )
-
-  # Rename in preparation for combining.
-  retval <- lapply(
-    seq_along(retval),
-    function(i, dat, postfixes) {
-      indices <- which(colnames(dat[[i]]) %in% c(method, "Meaningless"))
-      colnames(dat[[i]])[indices] <-
-        paste0(colnames(dat[[i]])[indices], postfixes[i])
-      dat[[i]]
-    },
-    dat = retval, postfixes = postfixes
-  )
-
-  # Combine
-  retval <- dplyr::full_join(
-    retval[[1]],
-    retval[[2]],
-    by = c(dplyr::all_of(indicator), "Bootstrap", "Type", "Patch1", "Patch2")
-  ) %>% dplyr::full_join(
-    retval[[3]],
-    by = c(dplyr::all_of(indicator), "Bootstrap", "Type", "Patch1", "Patch2")
-  ) %>% tidyr::pivot_longer(
-    cols = -c(dplyr::all_of(indicator), "Bootstrap", "Type", "Patch1", "Patch2"),
-    names_to = c("Measure", "Subset"),
-    names_sep = ", "
-  ) %>% tidyr::pivot_wider(
-    names_from = "Measure", values_from = "value"
-  )
-
-  return(retval)
-}
 
 bootstrapSamplesPairedBeta_Jaccard <- ConvertPreparedToBeta(
   bootstrapSamplesPairedBeta_source %>% dplyr::filter(
