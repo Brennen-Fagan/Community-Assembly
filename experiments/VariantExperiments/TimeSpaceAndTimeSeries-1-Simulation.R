a93c01a6fb1e9fa36bcdd40e04f37fa126679132
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2023-12-19 12:44:31 +0000

    Matrix Interpolation Function
    As described in-line, we pick a function that interpolates between two matrices that has a balance between how much transition occurs in the middle and how much the interpolation occurs at the edges.
    
    I've also suggested a form for applying the intervention, but details are unspecified at current because we need time dependence inside the per capita dynamics (both for if we decide to have a pool interpolation and for the matrix interpolation). This should be in the main trunk and then merged in here.

--- experiments/VariantExperiments/TimeSpaceAndTimeSeries-1-Simulation.R
+++ experiments/VariantExperiments/TimeSpaceAndTimeSeries-1-Simulation.R
@@ -167,4 +167,21 @@ stopifnot(length(.sFile1) == length(.sFile2),
 # but the samples we compare may be midway through or after interventions, esp.
 # when comparing slow to fast or immediate interventions.
 
+# Choose a sigmoidal interpolation between the matrices. 
+# Problem: we want no deviation at 0 or t = timespan, but not too much in mid.
+# Crit. Value = first numb. in tanh. 4 => 2% dev at edge, ~75% done in mid. 50%.
+# Require: Output is a function.
+# Require: Output: t = timespan / 2 => out = (mat1 + mat2) / 2.
+interpolateMatrices <- function(matrix1, matrix2, timespan) {
+  stopifnot(dim(matrix1) == dim(matrix2))
+  force(matrix1);force(matrix2);force(timespan)
+  function(t) {
+    matrix1 + (matrix2 - matrix1) * (tanh(4 * (t/timespan - 0.5)) + 1) / 2
+  }
+}
 
+simulateIntervention <- function(
+  base, time, interactionMatrixFunction, reproductionRateFunction
+) {
+  PerCapitaDynamics
+}
