6f465c83943dcdf6a112a1914a69723356211fc6
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2023-12-20 13:30:57 +0000

    Update TimeSpaceAndTimeSeries-1-Simulation.R
    Setting up for a test run. Loading files works. Interventions appear to now work in sandbox with appropriate start times. Implemented a basic set of sampling regimes.

--- experiments/VariantExperiments/TimeSpaceAndTimeSeries-1-Simulation.R
+++ experiments/VariantExperiments/TimeSpaceAndTimeSeries-1-Simulation.R
@@ -6,7 +6,7 @@
 
 # Create a Master seed, which we'll use to generate simulation seeds.
 # runif(1) * 1e8
-simulations <- 100
+simulationsNumber <- 10
 simulationsDictionaryChoice <- 1
 interventionChoice <-
   1. # The change does not occur, but the scientists don't know that!
@@ -16,34 +16,60 @@ interventionChoice <-
 # 5. # Two changes, one following 2, the other 4.
 # 6. # Two changes, one following 3, the other 4.
 
-cores <- 3
+cores <- 1
 
+# NOTE: Might be able to improve by splitting and collapsing Sources 1 and 2.
+# Goal => create a list of simulations, each represented as a list of files.
 simulationsDictionary <- data.frame(
   SourceSimulations = c(
     # Sets of files, as a string, that can be split with ", ".
     # NOTE: Files should be generated with same parameters for comparability.
     # NOTE: Use file.path if you need files in directories.
-    paste0(c(), sep = ", ")
+    paste0(c(
+      file.path("Data_2023-07-06",
+                "MNA-ExampleExtProp-Result-Env10-Ring-Inf-1-1-ExtProp1.RData"),
+      file.path("Data_2023-09-23",
+                "MNA-ExampleExtProp-Result-Env10-Ring-Inf-1-1-ExtProp1.RData"),
+      file.path("Data_2023-09-24",
+                "MNA-ExampleExtProp-Result-Env10-Ring-Inf-1-1-ExtProp1.RData"),
+      file.path("Data_2023-09-25",
+                "MNA-ExampleExtProp-Result-Env10-Ring-Inf-1-1-ExtProp1.RData"),
+      file.path("Data_2023-09-26",
+                "MNA-ExampleExtProp-Result-Env10-Ring-Inf-1-1-ExtProp1.RData")
+    ), collapse = ", ")
   ),
   SourceSimulations2 = c(
     # Sets of files, as a string, that can be split with ", ".
     # NOTE: Files here should be second halves of files in SourceSimulations.
-    # NOTE: Use NA if a corresponding second half does not exist.
+    # NOTE: Use "NA" if a corresponding second half does not exist.
     # NOTE: Use file.path if you need files in directories.
-    paste0(c(), sep = ", ")
+    paste0(c("NA", "NA", "NA", "NA", "NA"), collapse = ", ")
   ),
   SourcePools = c(
     # Sets of files, as a string, that can be split with ", ".
     # NOTE: Should correspond one-to-one with the SourceSimulations.
-    paste0(c(), sep = ", ")
+    paste0(c(
+      file.path("Data_2023-07-06",
+                "MNA-ExampleOutcome-PoolMats-Env10.RData"),
+      file.path("Data_2023-09-23",
+                "MNA-ExampleOutcome-PoolMats-Env10.RData"),
+      file.path("Data_2023-09-24",
+                "MNA-ExampleOutcome-PoolMats-Env10.RData"),
+      file.path("Data_2023-09-25",
+                "MNA-ExampleOutcome-PoolMats-Env10.RData"),
+      file.path("Data_2023-09-26",
+                "MNA-ExampleOutcome-PoolMats-Env10.RData")
+      ), collapse = ", ")
   ),
   Seeds = c(# runif(1) * 1e8
     69148611
   )
 )[simulationsDictionaryChoice, ]
 
+samplingMaxTime <- 250 # This is roughly twice the observed burn-in time from 0.
+samplingQuantity <- 100 # Not guaranteed!
+samplingTimeScaleLogarithmic <- TRUE
 calculationsPlotLong <- FALSE
-logarithmicTimeScale <- TRUE
 
 # Libraries: ##################################################################
 library(dplyr)
@@ -87,7 +113,7 @@ stopifnot(length(.sFile1) == length(.sFile2),
   id = seq_along(.sFile1),
   file1 = iterators::iter(.sFile1),
   file2 = iterators::iter(.sFile2),
-  fpool = iterators::iter(.sFile2)
+  fpool = iterators::iter(.sPool)
 ) %op% {
   simulation <- loadSimulation(file1, if(file2 != "NA") file2)
 
@@ -125,6 +151,15 @@ stopifnot(length(.sFile1) == length(.sFile2),
     # Note 3 & 4 are optional if all simulations of a row were saved together.
   }
 
+  lastTimeSampleable <- simulation$Events$Times[
+    length(simulation$Events$Times)
+    ]
+  lastTimeSampleable <-
+    lastTimeSampleable - samplingMaxTime * 2
+  # The * 2 makes sure we're also outside the burnout as well as that we have
+  # enough time for sampling.
+
+
   return(list(
     ID = id,
     File1 = file1,
@@ -133,7 +168,11 @@ stopifnot(length(.sFile1) == length(.sFile2),
     NEnvs = simulation$NumEnvironments,
     NSpec = nrow(Pool),
     Simulation = simulation,
-    Pool = Pool
+    Pool = Pool,
+    IntervenableTimes = c(
+      125, # Identified by inspection.
+      lastTimeSampleable
+    )
   ))
 }
 
@@ -167,32 +206,61 @@ stopifnot(length(.sFile1) == length(.sFile2),
 # but the samples we compare may be midway through or after interventions, esp.
 # when comparing slow to fast or immediate interventions.
 
-# Choose a sigmoidal interpolation between the matrices.
-# Problem: we want no deviation at 0 or t = timespan, but not too much in mid.
-# Crit. Value = first numb. in tanh. 4 => 2% dev at edge, ~75% done in mid. 50%.
-# Require: Output is a function.
-# Require: Output: t = timespan / 2 => out = (mat1 + mat2) / 2.
-interpolateMatrices <- function(matrix1, matrix2, timespan) {
-  stopifnot(dim(matrix1) == dim(matrix2))
-  force(matrix1);force(matrix2);force(timespan)
-  function(t, ...) {
-    matrix1 + (matrix2 - matrix1) * (tanh(4 * (t/timespan - 0.5)) + 1) / 2
-  }
+### Sampling Regime: ##########################################################
+# The times (with t = 0 == the intervention time) at which we should sample.
+if(samplingTimeScaleLogarithmic) {
+  # This version is symmetric on the log scale, centred on 1 time unit,
+  # and ends at the time gap. Number of sampling times not guaranteed.
+  samplingTimes <- c(0, unique(exp(c(
+    seq(from = 0,
+        to = -log(samplingMaxTime),
+        length.out = floor(samplingQuantity/2)),
+    seq(from = 0,
+        to = log(samplingMaxTime),
+        length.out = ceiling(samplingQuantity/2))
+  ))))
+} else {
+  samplingTimes <- seq(from = 0,
+                   by = samplingMaxTime/samplingQuantity,
+                   to = samplingMaxTime)
 }
-switchMatrices <- function(matrix1, matrix2, time) {
+
+### Interventions: ############################################################
+actTrivially <- function(elem) {force(elem); function(...){elem}}
+
+switchMatrices <- function(matrix1, matrix2, switchtime) {
   stopifnot(dim(matrix1) == dim(matrix2))
-  force(matrix1);force(matrix2);force(timespan)
+  force(matrix1);force(matrix2);force(switchtime)
   function(t, ...) {
-    if (t < time) {
+    if (t < switchtime) {
       matrix1
-    } else if (t > time) {
+    } else if (t > switchtime) {
       matrix2
     } else {
       (matrix1 + matrix2)/2
     }
   }
 }
+# Choose a sigmoidal interpolation between the matrices.
+# Problem: we want no deviation at 0 or t = timespan, but not too much in mid.
+# Crit. Value = first numb. in tanh. 4 => 2% dev at edge, ~75% done in mid. 50%.
+# Require: Output is a function.
+# Require: Output: t = timespan / 2 => out = (mat1 + mat2) / 2.
+interpolateMatrices <- function(matrix1, matrix2, timespan, switchtime = 0) {
+  stopifnot(dim(matrix1) == dim(matrix2))
+  force(matrix1);force(matrix2);force(timespan)
+  function(t, ...) {
+    # if (t < switchtime) {
+      # matrix1
+    # } else {
+      matrix1 + (matrix2 - matrix1) * (
+        tanh(4 * ( (t - switchtime) /timespan - 0.5)) + 1
+      ) / 2
+    # }
+  }
+}
 
+# Intervention Simulation: ####################################################
 # If we've coded things correctly, we should be able to re-run our original
 # RMTRCode2::MultipleNumericalAssembly_Dispersal(...) simulation.
 # Recall that this function
@@ -209,6 +277,14 @@ switchMatrices <- function(matrix1, matrix2, time) {
 #   1) pick a base file and history,
 #   2) reformat it to meet the MNAD criteria,
 #   3) create a PerCapitaDynamics function to pass to MNAD.
-# The last of these can have some preparation ahead of time.
 
+results <- foreach::foreach(
+  id = 1:simulationsNumber,
+  s = iterators::iter(.s),
+  .options.RNG = simulationsDictionary$Seeds
+) %dorng% {
+  # Pick a random time to start sampling from.
 
+  # Note to thin the sampling times if they are smaller than the minimum
+  # timelength difference in the data.
+}
