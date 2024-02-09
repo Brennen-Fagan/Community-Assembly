7ddb90b4097742f1da27ae62275f2c86187b5099
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2024-02-09 10:34:03 +0000

    First, Reorganize Files.
    The existing MCA.R file was overly large. This should help with documentation (and should probably be done for other files as well).

---
+++ R/CalculateTrophicStructure.R
@@ -0,0 +1,180 @@
+CalculateTrophicStructure <- function(
+  Pool,
+  NumEnvironments,
+  InteractionMatrices,
+  EliminationThreshold
+) {
+  # Borrowing from LM1996-NumPoolCom-FoodWebs-2021-07.Rmd
+  nrowPool <- nrow(Pool)
+  `%>%` <- magrittr::`%>%`
+  
+  # This function should be appliable row-wise to the results.
+  # One does need to remove the time column, as usual.
+  function(y) {
+    # Clean up anything not present.
+    y <- ifelse(y <= EliminationThreshold, 0, y)
+    
+    # Break y up into its environments.
+    EnvsY <- lapply(1:NumEnvironments, function(i, ys) {
+      ys[(i - 1) * nrowPool + 1:nrowPool]
+    }, ys = y)
+    
+    EnvsEdgeVertexLists <- lapply(
+      seq_along(EnvsY),
+      function(i, envY, mats) {
+        # "Red"uced "Com"munity; who's present.
+        redCom <- which(envY[[i]] > 0)
+        
+        if (length(redCom) == 0) {
+          return(list(
+            Edges = NA,
+            Vertices = NA
+          ))
+        }
+        
+        redPool <- Pool[redCom, ]
+        redMat <- matrix(mats[[i]][redCom, redCom],
+                         nrow = length(redCom),
+                         ncol = length(redCom))
+        
+        colnames(redMat) <- paste0('s',as.character(redCom))
+        rownames(redMat) <- colnames(redMat)
+        
+        names(redPool)[1] <- "node"
+        redPool$node <- colnames(redMat)
+        
+        Graph <- igraph::graph_from_adjacency_matrix(
+          redMat, weighted = TRUE
+        )
+        
+        Graph <- igraph::set.vertex.attribute(
+          Graph, "name", value = colnames(redMat)
+        )
+        
+        redPool$N <- envY[[i]][redCom]
+        
+        # For later analysis, take the matrix diagonal.
+        
+        redPool$Intraspecific <- diag(redMat)
+        
+        GraphAsDataFrame <- igraph::as_data_frame(Graph)
+        
+        # Add in abundances for calculating abundance * (gain or loss)
+        GraphAsDataFrame <- dplyr::left_join(
+          GraphAsDataFrame,
+          dplyr::select(redPool, node, N),
+          by = c("to" = "node")
+        )
+        
+        if (("weight" %in% colnames(GraphAsDataFrame))) {
+          # We're in a case where there are edges.
+          # In the opposite case, we cannot do this part of the calculation.
+          # Split data frame.
+          ResCon <- GraphAsDataFrame[GraphAsDataFrame$weight > 0,]
+          ConRes <- GraphAsDataFrame[GraphAsDataFrame$weight < 0,]
+          
+          # Reorder and rename variables.
+          ResCon <- dplyr::select(ResCon,
+                                  to, from, # resource = to, consumer = from,
+                                  effectPerUnit = weight, resourceAbund = N)
+          ConRes <- dplyr::select(ConRes,
+                                  to, from, # resource = from, consumer = to,
+                                  effectPerUnit = weight, consumerAbund = N)
+          ResCon <- dplyr::mutate(dplyr::group_by(ResCon, from),
+                                  effectActual = effectPerUnit * resourceAbund,
+                                  Type = "Exploit+")
+          ConRes <- dplyr::mutate(dplyr::group_by(ConRes, from),
+                                  effectActual = effectPerUnit * consumerAbund,
+                                  Type = ifelse(from == to,
+                                                "SelfReg-",
+                                                "Exploit-"))
+        }
+        
+        IntriG <- with(redPool, data.frame(
+          from = node, #resource = node,
+          to = node, #consumer = node,
+          effectPerUnit = ifelse(ReproductionRate > 0,
+                                 ReproductionRate, 0),
+          effectActual = ifelse(ReproductionRate > 0,
+                                N * ReproductionRate, 0),
+          Type = "Intrisc+",
+          stringsAsFactors = FALSE))
+        IntriL <- with(redPool, data.frame(
+          from = node, #resource = node,
+          to = node, #consumer = node,
+          effectPerUnit = ifelse(ReproductionRate < 0,
+                                 ReproductionRate, 0),
+          effectActual = ifelse(ReproductionRate < 0,
+                                N * ReproductionRate, 0),
+          Type = "Intrisc-",
+          stringsAsFactors = FALSE))
+        
+        if (exists("ResCon")) {
+          ResCon <- dplyr::select(ResCon, -resourceAbund)
+        } else {
+          ResCon <- data.frame()
+        }
+        if (exists("ConRes")) {
+          ConRes <- dplyr::select(ConRes, -consumerAbund)
+        } else {
+          ConRes <- data.frame()
+        }
+        
+        EdgeDataFrame <- dplyr::bind_rows(
+          ResCon, ConRes,
+          IntriG, IntriL
+        )
+        
+        EdgeDataFrame <- EdgeDataFrame %>% dplyr::rename(
+          # Empirically speaking, to and from appear reversed.
+          # A consumer (from) should have a negative effect on resource (to),
+          # but the organisation so far marks it as positive. We fix this.
+          tempname = to,
+          to = from
+        ) %>% dplyr::rename(
+          from = tempname
+        ) %>% dplyr::filter(
+          # Remove placeholder entries
+          effectPerUnit != 0
+        ) %>% dplyr::mutate(
+          # Useful to keep effects separate
+          effectSign = sign(effectPerUnit)
+        ) %>% dplyr::group_by(
+          to, effectSign
+        ) %>% dplyr::mutate(
+          # Perform the post mortem of the most influential from's
+          effectEfficiency = effectPerUnit / sum(effectPerUnit),
+          effectNormalised = effectActual / sum(effectActual)
+        ) %>% dplyr::arrange(to)
+        
+        list(
+          Edges = EdgeDataFrame,
+          Vertices = redPool
+        )
+        
+      },
+      envY = EnvsY,
+      mats = InteractionMatrices$Mats)
+    
+    EnvsCheddar <- lapply(EnvsEdgeVertexLists, RMTRCode2::toCheddar)
+    
+    EnvsTrophic <- lapply(EnvsCheddar,
+                          function(x, weight.by) {
+                            if (all(!is.na(x)))
+                              cheddar::TrophicLevels(x, weight.by = weight.by)
+                            else NA
+                          },
+                          weight.by = "effectNormalised")
+    
+    # In principle, I think these are the two return values.
+    # In practice, it seems more useful to return the EdgeVertexLists and
+    # the Trophic Levels, given the importance of intraspecific interactions.
+    # These are what Cheddar does not capture.
+    
+    return(list(
+      EdgeVertexLists = EnvsEdgeVertexLists,
+      TrophicLevels = EnvsTrophic
+    ))
+  }
+  
+}
---
+++ R/EliminationAndNeutralEvents.R
@@ -0,0 +1,54 @@
+# Note: This is a function that returns a function!
+EliminationAndNeutralEvents <- function(
+  EventsAndSeed, Species, #InteractionMatrices, #Pool,
+  PerCapitaDynamics, EliminationThreshold,
+  ArrivalDensity, ExtinctionProportion = 1, Verbose = FALSE
+) {
+  EventDF <- EventsAndSeed$Events # Other list entry is seed.
+  PerCapitaDynams <- PerCapitaDynamics
+  ArrivalDens <- ArrivalDensity
+  Verb <- Verbose
+  if (ExtinctionProportion <= 1)
+    RemainPercentage <- 1 - ExtinctionProportion
+  else
+    stop("ExtinctionProportion (= x) unclear. Please keep 0 <= x <= 1.")
+  
+  function(t, y, parms,
+           ReturnEvents = FALSE) {
+    if (ReturnEvents) {return(EventDF)}
+    if (Verb > 0) {
+      if (Verb == 1) {
+        print(paste("t:", t))
+      } else if (Verb > 1) {
+        print(paste(Sys.time(), "t:", t))
+      }
+    }
+    
+    y <- ifelse(y <= EliminationThreshold, 0, y)
+    event <- which(t == EventDF$Times)
+    if (length(event)) {
+      abundanceIndex <- (EventDF$Environment[event] - 1) * Species +
+        EventDF$Species[event]
+      if (EventDF$Type[event] == "Extinct") {
+        # Check if already present for records purposes.
+        EventDF$Success[event] <<- y[abundanceIndex] > 0
+        y[abundanceIndex] <- RemainPercentage * y[abundanceIndex]
+      } else if (EventDF$Type[event] == "Arrival") {
+        # Check if Uninvadable
+        # <=> per capita growth rate > 0
+        # <=> lim (epsilon -> 0) Dynamics(y + epsilon) > 0
+        # <=> PerCapitaDynamics > 0.
+        if (
+          y[abundanceIndex] > 0 ||
+          PerCapitaDynams(0, y, NULL)[abundanceIndex] > 0
+        ) {
+          y[abundanceIndex] <- y[abundanceIndex] + ArrivalDens
+          EventDF$Success[event] <<- TRUE
+        } else {
+          EventDF$Success[event] <<- FALSE
+        }
+      }
+    }
+    return(y)
+  }
+}
