2e4a2f29d1f2ab39fa9090222a5430e98aa09f91
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2024-03-15 11:36:59 +0000

    Update TimeSpaceAndTimeSeries-6c-Diversities.R
    Added call of abundance metrics. Going to test now.

--- experiments/VariantExperiments/TimeSpaceAndTimeSeries-6c-Diversities.R
+++ experiments/VariantExperiments/TimeSpaceAndTimeSeries-6c-Diversities.R
@@ -15,7 +15,8 @@ datfolders <- c(
   # "TSTS_Simulations_6-1_2-2_2024-02-15"
   # "TSTS_Simulations_11-1_3-3_2024-02-23",
   # "TSTS_Simulations_11-1_4-4_2024-02-23"
-  "TSTS_Simulations_17-1_5-5_2024-03-08"
+  "TSTS_Simulations_17-1_5-5_2024-03-08",
+  "TSTS_Simulations_18-1_6-6_2024-03-08"
 )
 
 cores <- 4
@@ -25,6 +26,8 @@ rows_per_event <- 1
 # Libraries: ##################################################################
 library(dplyr)
 library(RMTRCode2)
+
+source("TimeSpaceAndTimeSeries-0-Functions.R") # Abundance metrics.
 
 library(parallel)
 library(iterators)
@@ -214,6 +217,157 @@ Diversity <- foreach::foreach(
   return(Diversity)
 }
 
+DivAbund <- foreach::foreach(
+  x = iterators::iter(
+    dir(datfolders, full.names = TRUE,
+        pattern = "(Simulation|Result|Intervention)")
+  ), .packages = c("dplyr", "RMTRCode2")
+) %op% {
+  x_properties <- strsplit(basename(x), split = splitchar)
+  stopifnot(length(x_properties) == 1#,
+            #x_properties[[1]][1] == "TSTS",
+            #x_properties[[1]][2] == "Simulation"
+  )
+
+  filename <- file.path(
+    dirname(x),
+    if (flag == "TSTS") {
+      paste0(c(x_properties[[1]][1],
+               "DivAbund",
+               x_properties[[1]][3:length(x_properties[[1]])]), collapse = "_")
+    } else if (flag == "Data") {
+      paste0("TSTS_DivAbund_",
+             paste0(x_properties[[1]][5:length(x_properties[[1]])],
+                    collapse = "_"))
+    }
+  )
+
+  if(file.exists(filename)) {
+    load(filename)
+  } else {
+    print(filename)
+    x_dir <- dirname(x)
+    x_poolind <- which(unlist(lapply(poolmats, function(y) y$Dir == x_dir)))
+    if(any(x_poolind)) {
+      x_pool <- poolmats[[x_poolind]]$Pool
+    } else {
+      x_pool <- NULL
+    }
+
+    loaded <- load(x) # names
+    stopifnot(length(loaded) == 1)
+    loaded <- (get(loaded)) # objects
+
+    if (!"ReactionTime" %in% names(loaded$Ellipsis)) {
+      loaded$Ellipsis$ReactionTime <- loaded$ReactionTime
+    }
+
+    if (loaded$Ellipsis$Timescale == "Simulation") {
+      loaded$Events$Times <- loaded$Events$Times / loaded$Ellipsis$ReactionTime
+      loaded$Abundance[, 1] <- loaded$Abundance[, 1] / loaded$Ellipsis$ReactionTime
+      loaded$Ellipsis$Timescale <- "Characteristic"
+    }
+
+    if (!is.null(x_pool)) {
+      numberOfSpecies <- nrow(x_pool)
+    } else {
+      numberOfSpecies <- (ncol(loaded$Abundance) - 1) / loaded$NumEnvironments
+    }
+
+    if (!is.null(x_pool) &&
+        any(grepl(pattern = "(Niche|Affinity)", x = names(x_pool)))) {
+      # Proceed for each niche, then grab the overall.
+      Niches <- grep(pattern = "(Niche|Affinity)", x = names(x_pool),
+                     value = TRUE)
+
+      # This is the single niche implementation!!!
+      # We may want to, in the future, group_by all niches simultaneously.
+      # Alternatively, we may want to discretize the niches first.
+
+      x_pool <- x_pool %>% dplyr::mutate(
+        dplyr::across(
+          dplyr::all_of(Niches),
+          .fns = ~ if(length(unique(.x)) >
+                      ceiling((loaded$NumEnvironments + 1)/2)) {
+            # Where'd the formula come from? We have 10 patches, arranged in
+            # a ring. Assume that there are two patches that are maximally
+            # distant in niche space and the two paths are monotonic.
+            # These paths are of length n / 2 + 1 = 6. If we had 11, then the
+            # extra patch would be on adjacent to one of the extremes (w.l.o.g.)
+            # but then either one could be the extreme. So 11 also => 6.
+            cut(.x, breaks = ceiling((loaded$NumEnvironments + 1)/2))
+          } else {
+            .x
+          }
+        )
+      )
+
+      DivAbund <- lapply(
+        Niches, function(colname) {
+          x_pool %>% dplyr::group_by(
+            dplyr::across(dplyr::all_of(colname))
+          ) %>% dplyr::group_map(
+            .f = function(.x, .y) {
+              # .x$ID is numeric!
+              idcolumns <- c(
+                1,
+                rep(.x$ID, loaded$NumEnvironments) +
+                  1 +  # Time Column
+                  numberOfSpecies * rep((1:loaded$NumEnvironments) - 1,
+                                        each = length(.x$ID)) # Env Indexing
+              )
+              loaded_subset <- loaded
+              loaded_subset$Abundance <- loaded_subset$Abundance[, idcolumns]
+
+              divs <- thinAbundance(
+                loaded_subset$Abundance, loaded_subset$Events,
+                loaded_subset$Parameters$EliminationThreshold,
+                rows_per_event
+              ) %>% calculateAbundanceMetrics(
+                c(Basal = sum(.x$Type == "Basal"),
+                  Consumer = sum(.x$Type == "Consumer")),
+                loaded_subset$NumEnvironments
+              )
+
+              # thinAndCalculateDiversities yields lists
+              divs$NicheValues <- .y
+              return(divs)
+            }
+          )
+        }
+      )
+
+      names(DivAbund) <- Niches
+
+      DivAbund$DivAbund <- thinAbundance(
+        loaded$Abundance, loaded$Events,
+        loaded$Parameters$EliminationThreshold,
+        rows_per_event
+      ) %>% calculateAbundanceMetrics(
+        c(Basal = 34, Consumer = 66) * numberOfSpecies / 100,
+        loaded$NumEnvironments
+      )
+
+      DivAbund$Ellipsis <- loaded$Ellipsis
+    } else {
+      DivAbund <- list(
+        DivAbund = thinAbundance(
+          loaded$Abundance, loaded$Events,
+          loaded$Parameters$EliminationThreshold,
+          rows_per_event
+        ) %>% calculateAbundanceMetrics(
+          c(Basal = 34, Consumer = 66) * numberOfSpecies / 100,
+          loaded$NumEnvironments
+        ),
+        Ellipsis = loaded$Ellipsis
+      )
+    }
+
+    save(DivAbund, file = filename)
+  }
+
+  return(DivAbund)
+}
 
 SpeciesPresence <- foreach::foreach(
   x = iterators::iter(
