ccb551b34e5cb8a5a75d4ff1dd666ed7ee7eea02
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2024-03-15 10:54:58 +0000

    Adding a more generic Thin and Abundance metrics
    (In contrast to our binary/richness metrics that we've been using thus far.)
    
    At some point, these should probably be promoted in a grand refactoring and deprecation. For now, testing.

--- experiments/VariantExperiments/TimeSpaceAndTimeSeries-0-Functions.R
+++ experiments/VariantExperiments/TimeSpaceAndTimeSeries-0-Functions.R
@@ -671,6 +671,123 @@ ConvertPreparedToBeta <- function(
 }
 
 
+### Iterate ThinAndCalculate for non-binary: ##################################
+thinAbundance <- function(abundance, events, threshold,
+                          preferred_rows_per_event) {
+  bythin <- floor((nrow(abundance)
+                   / nrow(events))
+                  / preferred_rows_per_event)
+
+  abundance <- abundance[seq(from = 1,
+                             to = nrow(abundance),
+                             by = bythin), ]
+
+  # Remove illegal values (that the numerical engine uses as inbetweens).
+  toEliminate <- abundance[, -1] < threshold & abundance[, -1] > 0
+  abundance[, -1][toEliminate] <- 0
+
+  return(abundance)
+}
+
+# Like Calculating Diversities, but not for binary data only.
+calculateAbundanceMetrics <- function(abundance, nspecies, nenvironments) {
+  stopifnot(nenvironments >= 1)
+
+  envs <- lapply(
+    1:nenvironments,
+    function(i, abund, numSpecies) {
+      env <- abund[, 1 + 1:numSpecies + numSpecies * (i - 1)]
+    },
+    abund = abundance,
+    numSpecies = sum(nspecies)
+  )
+
+  time <- abund[, 1]
+
+  ### Alpha Diversity: ##################################################
+  diversity_alpha <- lapply(
+    1:nenvironments,
+    function(i, abund, numSpecies) {
+      env <- envs[[i]]
+      env_basal <- env[, 1:nspecies[1]]
+      env_consumer <- env[, nspecies[1] + 1:nspecies[2]]
+      metrics <- vegan::renyi(env, hill = TRUE)
+      metrics_basal <- vegan::renyi(env_basal, hill = TRUE)
+      metrics_consumer <- vegan::renyi(env_consumer, hill = TRUE)
+      names(metrics) <- paste0(names(metrics), ", All")
+      names(metrics_basal) <- paste0(names(metrics_basal), ", Basal")
+      names(metrics_consumer) <- paste0(names(metrics_consumer), ", Consumer")
+      cbind(Time = time,
+            metrics, metrics_basal, metrics_consumer,
+            Environment = i,
+            stringsAsFactors = FALSE)
+    },
+    abund = abundance,
+    numSpecies = sum(nspecies)
+  )
+
+  diversity_alpha <- dplyr::bind_rows(diversity_alpha)
+
+  print("alpha")
+  ### Gamma Diversity: ##################################################
+  # In contrast, need to combine across environments.
+  envgamma <- env[[1]]
+  if (nenvironments > 1) {
+    for (i in 2:nenvironments) {
+      envgamma <- envgamma + env[[i]]
+    }
+  }
+
+  env_basal <- envgamma[, 1:nspecies[1]]
+  env_consumer <- envgamma[, nspecies[1] + 1:nspecies[2]]
+
+  metrics <- vegan::renyi(envgamma, hill = TRUE)
+  metrics_basal <- vegan::renyi(env_basal, hill = TRUE)
+  metrics_consumer <- vegan::renyi(env_consumer, hill = TRUE)
+  names(metrics) <- paste0(names(metrics), ", All")
+  names(metrics_basal) <- paste0(names(metrics_basal), ", Basal")
+  names(metrics_consumer) <- paste0(names(metrics_consumer), ", Consumer")
+  diversity_gamma <- cbind(Time = time,
+        metrics, metrics_basal, metrics_consumer,
+        Environment = i,
+        stringsAsFactors = FALSE)
+
+  ### Beta Diversity (Jaccard, Space): ##################################
+  diversity_beta <- apply(
+    abundance,
+    MARGIN = 1, # Rows
+    function(row, envs) {
+      # Vegan complains about rows with all 0's.
+      # The warning is generic, so we cannot silence it specifically.
+      dists <- suppressWarnings(vegan::vegdist(
+        method = "bray",
+        x = matrix(row[-1], nrow = envs, byrow = TRUE)
+      ))
+
+      dataf <- expand.grid(
+        Env1 = 1:envs,
+        Env2 = 1:envs
+      ) %>% dplyr::filter(
+        Env1 < Env2
+      ) %>% dplyr::mutate(
+        Time = time,
+        BrayCurtis = dists
+      )
+
+      return(dataf)
+    },
+    envs = nenvironments
+  )
+  print("beta")
+
+  ### Return Diversities: ###############################################
+  return(list(
+    alpha = diversity_alpha,
+    beta = diversity_beta,
+    gamma = diversity_gamma
+  ))
+}
+
 ### Biodiversity Intactness: ##################################################
 
 # if (calculationsBII) {
