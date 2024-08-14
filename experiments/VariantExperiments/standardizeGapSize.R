59ced2632854a0b5b4abe3e0404c21feb4b3b1e6
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2024-08-14 14:50:42 +0100

    Create standardizeGapSize.R
    The alternative is to just forge ahead by guessing at a step size, but the minimum, and indeed most small step sizes including min(diff(...)), are too small, requiring too much memory to handle. There are some creative ways around this -- such as replacing things with function calls -- but I'm not sure how that would interact with the downstream functions that I'm trying to use from the stableGR package. As such, I'm constrained by working memory available in addition to any other computational problems, such as the error that occurred while computing n.eff.

---
+++ experiments/VariantExperiments/standardizeGapSize.R
@@ -0,0 +1,44 @@
+standardizeGapSize <- function(abundance, gap, epsilon = NULL) {
+  # format: abundance <- cbind(Time column, species densities)
+  # format: gap <- desired step size
+  # format: epsilon = NULL
+  if(!is.null(epsilon)) {
+    abundance[, -1][abundance[, -1] < epsilon] <- 0
+  }
+
+  timespan <- range(abundance[, 1])
+  timesout <- seq(timespan[1], timespan[2], by = gap)
+  splines <- lapply(2:ncol(abundance), function(col) {
+    spline(abundance[, 1], abundance[, col], xout = timesout)$y
+  })
+
+  retval <- cbind(timesout, do.call(cbind, splines))
+  if(!is.null(epsilon)) {
+    retval[, -1][retval[, -1] < epsilon] <- 0
+  }
+  return(retval)
+}
+
+datfolder <-
+"TSTS_Simulations_18-1_9-9_2024-08-06"
+results <- lapply(dir(datfolder, "Simulation", full.names = T), function(x) {nm <- load(x); return(get(nm))})
+preferredGap <- quantile(unlist(lapply(results, function(r) diff(r$Abundance[, 1]))), p = 0.03)
+temp <- lapply(
+  seq_along(results),
+  function(i) standardizeGapSize(
+    results[[i]]$Abundance,
+    gap = preferredGap,
+    epsilon = results[[i]]$Parameters$EliminationThreshold
+  )
+)
+min(unlist(lapply(temp, nrow))) -> minnrow
+nonzeros2 <- lapply(temp, function(x) which(!(apply(x, MARGIN = 2, var) == 0)))
+nonzeros2 <- Reduce(intersect, nonzeros2)
+nonzeros2
+stableGR::n.eff(lapply(temp, function(x) x[1:minnrow, nonzeros2[-1]]))
+
+# Error in solve.default(Tee, W) :
+#   system is computationally singular: reciprocal condition number = 6.49124e-19
+#???
+
+stableGR::stable.GR(lapply(temp, function(x) x[1:minnrow, nonzeros2[-1]]))
