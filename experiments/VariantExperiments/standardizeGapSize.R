0dc964f072b23e5b769f9c1ff8fc0234c3db2b6c
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2024-08-16 16:28:54 +0100

    Update standardizeGapSize.R
    Added Timespans to make truncating and burning in easier.

--- experiments/VariantExperiments/standardizeGapSize.R
+++ experiments/VariantExperiments/standardizeGapSize.R
@@ -1,4 +1,4 @@
-standardizeGapSize <- function(abundance, gap, epsilon = NULL) {
+standardizeGapSize <- function(abundance, gap, timespan = NULL, epsilon = NULL) {
   # format: abundance <- cbind(Time column, species densities)
   # format: gap <- desired step size
   # format: epsilon = NULL
@@ -6,7 +6,9 @@ standardizeGapSize <- function(abundance, gap, epsilon = NULL) {
     abundance[, -1][abundance[, -1] < epsilon] <- 0
   }
 
-  timespan <- range(abundance[, 1])
+  if (is.null(timespan)) {
+    timespan <- range(abundance[, 1])
+  }
   timesout <- seq(timespan[1], timespan[2], by = gap)
   splines <- lapply(2:ncol(abundance), function(col) {
     spline(abundance[, 1], abundance[, col], xout = timesout)$y
@@ -20,25 +22,39 @@ standardizeGapSize <- function(abundance, gap, epsilon = NULL) {
 }
 
 datfolder <-
-"TSTS_Simulations_18-1_9-9_2024-08-06"
-results <- lapply(dir(datfolder, "Simulation", full.names = T), function(x) {nm <- load(x); return(get(nm))})
-preferredGap <- quantile(unlist(lapply(results, function(r) diff(r$Abundance[, 1]))), p = 0.03)
+  "TSTS_Simulations_18-1_9-9_2024-08-06"
+results <-
+  lapply(dir(datfolder, "Simulation", full.names = T),
+         function(x) {nm <- load(x); return(get(nm))})
+preferredGap <-
+  quantile(unlist(lapply(results,
+                         function(r) diff(r$Abundance[, 1]))), p = 0.03)
+preferredTimes <-
+  c(500, min(unlist(lapply(results,
+                    function(r) r$Abundance[nrow(r$Abundance), 1]/2))))
+
 temp <- lapply(
   seq_along(results),
   function(i) standardizeGapSize(
     results[[i]]$Abundance,
     gap = preferredGap,
+    timespan = preferredTimes,
     epsilon = results[[i]]$Parameters$EliminationThreshold
   )
 )
+
 min(unlist(lapply(temp, nrow))) -> minnrow
 nonzeros2 <- lapply(temp, function(x) which(!(apply(x, MARGIN = 2, var) == 0)))
 nonzeros2 <- Reduce(intersect, nonzeros2)
 nonzeros2
-stableGR::n.eff(lapply(temp, function(x) x[1:minnrow, nonzeros2[-1]]))
+estimate_neff <-
+  stableGR::n.eff(lapply(temp, function(x) x[1:minnrow, nonzeros2[-1]]))
 
 # Error in solve.default(Tee, W) :
 #   system is computationally singular: reciprocal condition number = 6.49124e-19
 #???
 
-stableGR::stable.GR(lapply(temp, function(x) x[1:minnrow, nonzeros2[-1]]))
+estimate_GR <-
+  stableGR::stable.GR(lapply(temp, function(x) x[1:minnrow, nonzeros2[-1]]))
+# Works, but takes a while!
+# Returns are a bit strange: mpsrf < 1.
