0c4fa816679d244f210e18c15ecf0610e88d0450
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2024-08-20 12:16:26 +0100

    Update SketchComparisons.R
    Determined there was a bug with the way I was assigning time to the case of ParentRun, "After". Since my interventions were shifted to start at 0, the ParentRun, "After" case was being evaluated at the ParentRun, "Before" times. The InterventionRun cases were shifted appropriately (lines 38 - 46), and the ParentRun, "Before" cases are unaffected (because there is no shift).
    
    I've added a pair of new columns that reverse the shift for the ParentRun, "After" case specifically, and reduced the number of evaluations of the abundance functions by half. I've also separated out the simulations so that we can see clearer striping in the plots, rather than the noise of different color stripes overlaid on each other.

--- experiments/VariantExperiments/SketchComparisons.R
+++ experiments/VariantExperiments/SketchComparisons.R
@@ -15,10 +15,10 @@ results <- lapply(seq_along(results), function(i, r, n) {
   r[[i]]$RunName <- n[i]
   return(r[[i]])
 }, r = results, n = dir(datfolder, "Simulation", full.names = T))
-names(results) <- dir(datfolder, "Simulation", full.names = T) 
-names(interventions) <- dir(datfolder, "Intervention", full.names = T) 
+names(results) <- dir(datfolder, "Simulation", full.names = T)
+names(interventions) <- dir(datfolder, "Intervention", full.names = T)
 
-numberAttempts <- 10000 # 30,000 simulation scale time units (simts) TS~8 simts 
+numberAttempts <- 10000 # 30,000 simulation scale time units (simts) TS~8 simts
 # For non-"suitability", this is the number of equispaced chronological points
 # used for determining how the relationship develops through time.
 # For "suitability", this is the number of random samples we take to determine
@@ -26,27 +26,27 @@ numberAttempts <- 10000 # 30,000 simulation scale time units (simts) TS~8 simts
 
 # Note:
 stopifnot(
-  all(results[[1]]$ReactionTime == 
+  all(results[[1]]$ReactionTime ==
         unlist(lapply(c(results, interventions), function(r) r$ReactionTime)))
   )
 # So it doesn't matter which timescale we use, but it does matter start and end
 # times.
 # We have unperturbed-before, unperturbed-after, and perturbed-after.
-unperturbedRange <- 
+unperturbedRange <-
   c(500, min(unlist(lapply(interventions, function(r) r$Abundance[1, 1]))))
 
 # Work in TimeSincePerturbation units.
 interventions <- lapply(
-  interventions, 
+  interventions,
   function(r) {
-    r$Abundance[, 1] <- 
+    r$Abundance[, 1] <-
       (r$Abundance[, 1] - r$Ellipsis$Affinity$TimeIntervention)
     return(r)
   }
 )
-perturbedRange <- 
+perturbedRange <-
   c(0, min(unlist(lapply(
-    interventions, 
+    interventions,
     function(r) {
       (r$Events$Times[length(r$Events$Times)]
         - r$Ellipsis$Affinity$TimeIntervention)
@@ -57,7 +57,7 @@ perturbedRange <-
 # 1 aaaaaaaaaaaaaaaaaaaaaaaaaa/bbbbbbbbbbbbbbbbbbbbbbb Perturbation
 # 2 cccccccccccccccccccccccccc/ccccccccccccccccccccccc Spatial Control (Theory)
 # 3 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa Unperturbed
-# 4 cccccccccccccccccccccccccccccccccccccccccccccccccc Spatial Control 
+# 4 cccccccccccccccccccccccccccccccccccccccccccccccccc Spatial Control
 # Comparing 1 after / to 3 after /  -> True Perturbation Effect on focal patch.
 # Comparing 2 after / to 4 after /  -> True Perturbation Effect on non-focal.
 # Comparing 1 before / to 3 after / -> Temporal Control Suitability.
@@ -72,7 +72,7 @@ extractAbund <- function(abundance, nEnv, epsilon = NULL) {
   function(env, time) {
     timerow <- which.max(time < abundance[, 1]) - 1
     stopifnot(env >= 1, env <= nEnv, timerow >= 1, timerow <= nrow(abundance))
-    retval <- 
+    retval <-
       abundance[timerow, (env - 1) *nSpec + 1:nSpec + 1] # + 1 for Time Column
     if (is.null(epsilon)) {
       return(retval)
@@ -114,18 +114,21 @@ extractBrayDissimilarity <- function(ab1, ab2) {
 # TODO CHANGE FOR OTHER CONFIGURATIONS.
 interventionDetails <- data.frame(
   InterventionRun = names(interventions),
+  InterventionTime = unlist(lapply(
+    interventions, function(r) r$Ellipsis$Affinity$TimeIntervention
+  )),
   EnvIntervention = unlist(lapply(
     interventions, function(r) r$Ellipsis$Affinity$PatchInterventions
     )),
-  EnvControl = unlist(lapply( 
+  EnvControl = unlist(lapply(
     interventions,
-    function(r) setdiff(1:r$NumEnvironments, 
+    function(r) setdiff(1:r$NumEnvironments,
                         r$Ellipsis$Affinity$PatchInterventions)
   )),
   stringsAsFactors = FALSE
 )
 
-# From above, but 2 + 4 -> ControlEnv, 1 + 3 InterventionEnv, 
+# From above, but 2 + 4 -> ControlEnv, 1 + 3 InterventionEnv,
 #                 1 + 2 after is InterventionRun
 # Comparing 1 after / to 3 after /  -> True Focal.
 # Comparing 2 after / to 4 after /  -> True Nonfocal.
@@ -133,11 +136,11 @@ interventionDetails <- data.frame(
 # Comparing 2 after / to 3 after /  -> Spatial Suitability.
 # Comparing 1 before / to 1 after / -> Temporal Control.
 # Comparing 2 after / to 1 after /  -> Spatial Control.
-comparisonTypes <- c("True Focal", 
-                     "True Nonfocal", 
-                     "Temporal Suitability", 
+comparisonTypes <- c("True Focal",
+                     "True Nonfocal",
+                     "Temporal Suitability",
                      "Spatial Suitability",
-                     "Temporal Control", 
+                     "Temporal Control",
                      "Spatial Control")
 
 abundances <- lapply(c(results, interventions), function(r) {
@@ -151,11 +154,11 @@ attempts <- expand.grid(
   Replicate = 1:numberAttempts,
   ParentRun = names(results),
   InterventionType = c("12-1-p-2_1-1", "12-1-p-3_1-1"),
-  ComparisonType = comparisonTypes, 
+  ComparisonType = comparisonTypes,
   stringsAsFactors = FALSE
 ) %>% dplyr::mutate(
-  InterventionRun = 
-    paste0(gsub(pattern = "Simulation_", 
+  InterventionRun =
+    paste0(gsub(pattern = "Simulation_",
                 replacement = "Intervention_",
                 x = tools::file_path_sans_ext(ParentRun)),
            "_", InterventionType, ".RData")
@@ -163,87 +166,98 @@ attempts <- expand.grid(
   interventionDetails, by = "InterventionRun"
 ) %>% dplyr::mutate(
   Env1 = dplyr::case_when(
-    ComparisonType == comparisonTypes[1] ~ EnvIntervention, 
-    ComparisonType == comparisonTypes[2] ~ EnvControl, 
-    ComparisonType == comparisonTypes[3] ~ EnvIntervention, 
+    ComparisonType == comparisonTypes[1] ~ EnvIntervention,
+    ComparisonType == comparisonTypes[2] ~ EnvControl,
+    ComparisonType == comparisonTypes[3] ~ EnvIntervention,
     ComparisonType == comparisonTypes[4] ~ EnvControl,
-    ComparisonType == comparisonTypes[5] ~ EnvIntervention, 
+    ComparisonType == comparisonTypes[5] ~ EnvIntervention,
     ComparisonType == comparisonTypes[6] ~ EnvControl,
     TRUE ~ NA_integer_),
   Env2 = dplyr::case_when(
-    ComparisonType == comparisonTypes[1] ~ EnvIntervention, 
-    ComparisonType == comparisonTypes[2] ~ EnvControl, 
-    ComparisonType == comparisonTypes[3] ~ EnvIntervention, 
+    ComparisonType == comparisonTypes[1] ~ EnvIntervention,
+    ComparisonType == comparisonTypes[2] ~ EnvControl,
+    ComparisonType == comparisonTypes[3] ~ EnvIntervention,
     ComparisonType == comparisonTypes[4] ~ EnvIntervention,
-    ComparisonType == comparisonTypes[5] ~ EnvIntervention, 
+    ComparisonType == comparisonTypes[5] ~ EnvIntervention,
     ComparisonType == comparisonTypes[6] ~ EnvIntervention,
     TRUE ~ NA_integer_),
   Env1File = dplyr::case_when(
-    ComparisonType == comparisonTypes[1] ~ InterventionRun, 
-    ComparisonType == comparisonTypes[2] ~ InterventionRun, 
-    ComparisonType == comparisonTypes[3] ~ ParentRun, 
+    ComparisonType == comparisonTypes[1] ~ InterventionRun,
+    ComparisonType == comparisonTypes[2] ~ InterventionRun,
+    ComparisonType == comparisonTypes[3] ~ ParentRun,
     ComparisonType == comparisonTypes[4] ~ InterventionRun,
-    ComparisonType == comparisonTypes[5] ~ ParentRun, 
+    ComparisonType == comparisonTypes[5] ~ ParentRun,
     ComparisonType == comparisonTypes[6] ~ InterventionRun,
     TRUE ~ NA_character_),
   Env2File = dplyr::case_when(
-    ComparisonType == comparisonTypes[1] ~ ParentRun, 
-    ComparisonType == comparisonTypes[2] ~ ParentRun, 
-    ComparisonType == comparisonTypes[3] ~ ParentRun, 
+    ComparisonType == comparisonTypes[1] ~ ParentRun,
+    ComparisonType == comparisonTypes[2] ~ ParentRun,
+    ComparisonType == comparisonTypes[3] ~ ParentRun,
     ComparisonType == comparisonTypes[4] ~ ParentRun,
-    ComparisonType == comparisonTypes[5] ~ InterventionRun, 
+    ComparisonType == comparisonTypes[5] ~ InterventionRun,
     ComparisonType == comparisonTypes[6] ~ InterventionRun,
     TRUE ~ NA_character_),
-  Env1Abundance = unlist(lapply(Env1File, 
-                                function(f) which(f == names(abundances)))), 
-  Env2Abundance = unlist(lapply(Env2File, 
+  Env1Abundance = unlist(lapply(Env1File,
+                                function(f) which(f == names(abundances)))),
+  Env2Abundance = unlist(lapply(Env2File,
                                 function(f) which(f == names(abundances)))),
   Env1Timespan = dplyr::case_when(
-    ComparisonType == comparisonTypes[1] ~ "After", 
-    ComparisonType == comparisonTypes[2] ~ "After", 
-    ComparisonType == comparisonTypes[3] ~ "Before", 
+    ComparisonType == comparisonTypes[1] ~ "After",
+    ComparisonType == comparisonTypes[2] ~ "After",
+    ComparisonType == comparisonTypes[3] ~ "Before",
     ComparisonType == comparisonTypes[4] ~ "After",
-    ComparisonType == comparisonTypes[5] ~ "Before", 
+    ComparisonType == comparisonTypes[5] ~ "Before",
     ComparisonType == comparisonTypes[6] ~ "After",
     TRUE ~ NA_character_),
   Env2Timespan = dplyr::case_when(
-    ComparisonType == comparisonTypes[1] ~ "After", 
-    ComparisonType == comparisonTypes[2] ~ "After", 
-    ComparisonType == comparisonTypes[3] ~ "After", 
+    ComparisonType == comparisonTypes[1] ~ "After",
+    ComparisonType == comparisonTypes[2] ~ "After",
+    ComparisonType == comparisonTypes[3] ~ "After",
     ComparisonType == comparisonTypes[4] ~ "After",
-    ComparisonType == comparisonTypes[5] ~ "After", 
+    ComparisonType == comparisonTypes[5] ~ "After",
     ComparisonType == comparisonTypes[6] ~ "After",
     TRUE ~ NA_character_),
   Env1Time = dplyr::case_when(
-    Env1Timespan == Env2Timespan & Env1Timespan == "Before" ~ 
-      unperturbedRange[1] + 
+    Env1Timespan == Env2Timespan & Env1Timespan == "Before" ~
+      unperturbedRange[1] +
       diff(unperturbedRange)/(numberAttempts - 1) * (Replicate - 1),
-    Env1Timespan == Env2Timespan & Env1Timespan == "After" ~ 
-      perturbedRange[1] + 
+    Env1Timespan == Env2Timespan & Env1Timespan == "After" ~
+      perturbedRange[1] +
       diff(perturbedRange)/(numberAttempts - 1) * (Replicate - 1),
-    Env1Timespan == "Before" ~ runif(n = length(Env1Timespan), 
+    Env1Timespan == "Before" ~ runif(n = length(Env1Timespan),
                                      min = unperturbedRange[1],
                                      max = unperturbedRange[2]),
-    Env1Timespan == "After" ~ runif(n = length(Env1Timespan), 
+    Env1Timespan == "After" ~ runif(n = length(Env1Timespan),
                                      min = perturbedRange[1],
                                      max = perturbedRange[2]),
     TRUE ~ NA_real_
   ),
   Env2Time = dplyr::case_when(
-    Env1Timespan == Env2Timespan & Env2Timespan == "Before" ~ 
-      unperturbedRange[1] + 
+    Env1Timespan == Env2Timespan & Env2Timespan == "Before" ~
+      unperturbedRange[1] +
       diff(unperturbedRange)/(numberAttempts - 1) * (Replicate - 1),
-    Env1Timespan == Env2Timespan & Env2Timespan == "After" ~ 
-      perturbedRange[1] + 
+    Env1Timespan == Env2Timespan & Env2Timespan == "After" ~
+      perturbedRange[1] +
       diff(perturbedRange)/(numberAttempts - 1) * (Replicate - 1),
-    Env2Timespan == "Before" ~ runif(n = length(Env2Timespan), 
+    Env2Timespan == "Before" ~ runif(n = length(Env2Timespan),
                                      min = unperturbedRange[1],
                                      max = unperturbedRange[2]),
-    Env2Timespan == "After" ~ runif(n = length(Env2Timespan), 
+    Env2Timespan == "After" ~ runif(n = length(Env2Timespan),
                                     min = perturbedRange[1],
                                     max = perturbedRange[2]),
     TRUE ~ NA_real_
-  )#,
+  ),
+  Env1TimeEval = dplyr::case_when(
+    Env1Timespan == "After" &
+      Env1File == ParentRun ~ Env1Time + InterventionTime,
+    TRUE ~ Env1Time
+  ),
+  Env2TimeEval = dplyr::case_when(
+    Env2Timespan == "After" &
+      Env2File == ParentRun ~ Env2Time + InterventionTime,
+    TRUE ~ Env2Time
+  )
+  #,
   # Jaccard = NA,
   # Bray = NA
 )
@@ -252,14 +266,10 @@ attempts <- attempts %>% dplyr::group_by(# Rowwise
   Replicate, ParentRun, InterventionType, ComparisonType
 ) %>% dplyr::group_modify(
   .f = function(.x, .y) {# non-group cols, group cols.
-    jacc <- extractJaccDissimilarity(
-      abundances[[.x$Env1Abundance]](.x$Env1, .x$Env1Time),
-      abundances[[.x$Env2Abundance]](.x$Env2, .x$Env2Time)
-      )
-    bray <- extractBrayDissimilarity(
-      abundances[[.x$Env1Abundance]](.x$Env1, .x$Env1Time),
-      abundances[[.x$Env2Abundance]](.x$Env2, .x$Env2Time)
-    )
+    ab1 <- abundances[[.x$Env1Abundance]](.x$Env1, .x$Env1TimeEval)
+    ab2 <- abundances[[.x$Env2Abundance]](.x$Env2, .x$Env2TimeEval)
+    jacc <- extractJaccDissimilarity(ab1, ab2)
+    bray <- extractBrayDissimilarity(ab1, ab2)
     return(.x %>% dplyr::mutate(
       Jaccard = jacc, Bray = bray
     ))
@@ -267,17 +277,17 @@ attempts <- attempts %>% dplyr::group_by(# Rowwise
 ) %>% dplyr::ungroup()
 
 ggplot2::ggplot(
-  attempts, 
+  attempts,
   ggplot2::aes(x = Env1Time, y = Env2Time, color = Jaccard)
 ) + ggplot2::geom_point(
-) + ggplot2::facet_wrap(
-  .~ComparisonType
-) + ggplot2::scale_color_viridis_b(breaks = (0:7)/7) 
+) + ggplot2::facet_grid(
+  ComparisonType ~ ParentRun + InterventionType
+) + ggplot2::scale_color_viridis_c()
 
 ggplot2::ggplot(
-  attempts, 
+  attempts,
   ggplot2::aes(x = Env1Time, y = Env2Time, color = Bray)
 ) + ggplot2::geom_point(
-) + ggplot2::facet_wrap(
-  .~ComparisonType
-) + ggplot2::scale_color_viridis_b(breaks = (0:7)/7) 
+) + ggplot2::facet_grid(
+  ComparisonType ~ ParentRun + InterventionType
+) + ggplot2::scale_color_viridis_c()
