6c2e588f8ef9e14594610f8aebd93b2b9ef2e5d8
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2025-02-27 10:05:46 +0000

    Update TimeSpaceAndTimeSeries-0-Functions.R
    Added calculateColExtMetrics, which recalculates the events object so that it contains only successful observations, but also includes dynamic as well as neutral events.

--- experiments/VariantExperiments/TimeSpaceAndTimeSeries-0-Functions.R
+++ experiments/VariantExperiments/TimeSpaceAndTimeSeries-0-Functions.R
@@ -915,8 +915,8 @@ calculateDiversityMetrics <- function(abundance, nspecies, nenvironments) {
 
     diversityGamma <-
       hillWrapper(env = envgamma, i = NA) %>% dplyr::mutate(
-      Metric = paste0("Gamma ", Metric)
-    )
+        Metric = paste0("Gamma ", Metric)
+      )
     diversityGamma <- cbind(
       Time = rep(time, each = nrow(diversityGamma)/length(time)),
       diversityGamma
@@ -1124,6 +1124,147 @@ calculateAbundanceMetrics <- function(abundance, nspecies, nenvironments) {
     beta = diversity_beta,
     gamma = diversity_gamma
   ))
+}
+
+calculateColExtMetrics <- function(sim) {
+  # Might be easier to take the binary matrix and to say
+  # 0->1 and in the successful events ~ colonization
+  # 0->1 and not in the successful events ~ dispersal
+  # 1->0 and in the successful events ~ neutral extirpation
+  # 1->0 and not in the successful events ~ dynamic extirpation
+  # But note that more than one of these can co-occur.
+  binaryMatrix <-
+    sim$Abundance[, -1] > sim$Parameters$EliminationThreshold
+  ColExtMatrix <- # Next - Current so timing of event lines up with the change
+    binaryMatrix[2:nrow(sim$Abundance), ] -
+    binaryMatrix[1:(nrow(sim$Abundance) - 1), ]
+
+  nspecies <- ncol(ColExtMatrix)
+
+  # Reattach the times
+  ColExtMatrix <- cbind(sim$Abundance[1:(nrow(sim$Abundance) - 1), 1],
+                        ColExtMatrix)
+
+
+  allEvents <- lapply(1:nrow(ColExtMatrix), function(i, mat, events) {
+    time <- mat[i, 1]
+    changes <- which(mat[i, -1] != 0)
+    event <- events %>% dplyr::filter(Times == time, Success) # N.B. nrow can >1
+
+    if (nrow(event) == 0 && length(changes) == 0) {
+      # Done, nothing to report.
+      return(NULL)
+    }
+
+    environments <- ((changes - 1) %/% nspecies) + 1 # 1:20 w/10 -> 10 1s, 10 2s
+    species <- ((changes - 1) %% nspecies) + 1 # 1:20 w/10 -> c(1:10, 1:10)
+    stopifnot(environments <= result$NumEnvironments)
+
+    if (nrow(event) == 1 && length(changes) == 1 &&
+        event$Species == species && event$Environment == environments &&
+        ((mat[i, changes + 1] == 1 && event$Type == "Arrival") ||
+         (mat[i, changes + 1] == -1 && event$Type == "Extinct")
+        )) {
+      # Trivial, everything is already recorded
+      return(event)
+    }
+
+    # Bundle information together for easier processing.
+    changesdf <- data.frame(
+      change = changes,
+      type = mat[i, changes+1],
+      species = species,
+      environments = environments
+    ) %>% dplyr::mutate(index = dplyr::row_number())
+
+    # Else: complicated, some combination of events or non-neutral events.
+    # Proceed along events, removing any changes that are already accounted for.
+    # (Essentially, we're doing a complicated implementation of a filtering join
+    # where we're trying to filter out things that have already been addressed.)
+    # Simultaneously, if an event occurs and the result is not registered as a
+    # change, then something must have countered it. If neutral events cannot
+    # occur simultaneously, then it must be a non-neutral event.
+    if (nrow(event) > 0) {
+      for (j in 1:nrow(event)) {
+        thisEvent <- event[j, ]
+        thisChange <- changesdf %>% dplyr::filter(
+          species == thisEvent$Species,
+          environments == thisEvent$Environment
+        )
+
+        if (nrow(thisChange) == 1 &&
+            ((thisChange$change == 1 && thisEvent$Type == "Arrival") ||
+             (thisChange$change == -1 && thisEvent$Type == "Extinct")
+            )) {
+          # This change is already in event(s) as this event.
+          # Remove it from the data.frame of event(s).
+          changesdf <- changesdf %>% dplyr::filter(index != thisChange$index)
+        } else if (
+          nrow(thisChange) == 0 # then an event happened, but no change recorded.
+        ) {
+          event <- rbind(event, data.frame(
+            Times = time,
+            Species = thisEvent$Species,
+            Environment = thisEvent$Environment,
+            Type = switch(thisEvent$Type,
+                          "Arrival" = "Dynamic Loss", # Should've come, but no
+                          "Extinct" = "Dispersal" # Should've gone, but here
+            ),
+            Success = TRUE
+          ))
+        }
+      }
+    }
+
+    # Proceed along changes, adding them to events.
+    if (nrow(changesdf) > 0) {
+      for (j in 1:nrow(changesdf)) {
+        thisChange <- changesdf[j, ]
+        thisEvent <- event %>% dplyr::filter(
+          Species == thisChange$species,
+          Environment == thisChange$environments
+        )
+        if (nrow(thisEvent) == 0) {
+          # Event definitely not recorded, create a new one and add it.
+          event <- rbind(event, data.frame(
+            Times = time,
+            Species = thisChange$species,
+            Environment = thisChange$environments,
+            Type = switch(thisChange$type + 2, # -1:1 + 2 -> 1:3
+                          "Dynamic Loss",
+                          "Oops",
+                          "Dispersal"
+            ),
+            Success = TRUE
+          ))
+        } else if (nrow(thisEvent) == 1 &&
+                   thisEvent$Species == thisChange$species &&
+                   thisEvent$Environment == thisChange$environments &&
+                   ((thisChange$type == 1 && thisEvent$Type == "Arrival") ||
+                    (thisChange$type == -1 && thisEvent$Type == "Extinct")
+                   )) {
+          # Trivial, everything is already recorded.
+          # Note this also should already have been caught!
+          next()
+        } else {
+          # Event recorded, but opposite of what is observed -- undone twice.
+          # Assuming that events take place at distinct times, which should be
+          # guaranteed from the code we are using, this shouldn't happen.
+          stop("Double-check assumptions: a 'triple event' occurred.")
+        }
+      }
+    }
+
+    return(event)
+  },
+  mat = ColExtMatrix,
+  events = result$Events)
+
+  allEvents <- dplyr::bind_rows(allEvents) %>% dplyr::arrange(Times)
+  # Adding in typings is then dependent on how we are breaking up affinity
+  # and what information from the pool is desired. I think it might make
+  # more sense to do that outside of this function.
+  return(allEvents)
 }
 
 ### Biodiversity Intactness: ##################################################
