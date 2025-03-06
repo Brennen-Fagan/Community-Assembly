fd6f971e35ec6145f10181cf7d79404f98624fcb
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2025-03-06 14:26:08 +0000

    Update trialImages_MWE_2025-01-17.R
    Additional Useful Categories + Images for single runs.

--- experiments/VariantExperiments/trialImages_MWE_2025-01-17.R
+++ experiments/VariantExperiments/trialImages_MWE_2025-01-17.R
@@ -5,6 +5,93 @@ library(dplyr)
 load("div_2024-11-30-341.RData")
 load("pres_2024-11-30-341.RData")
 
+divTemp <- divTemp %>% dplyr::mutate(
+  SpeciesAffinity = dplyr::case_when(
+    SpeciesAffinity == "rep_0" ~ "100% 0",
+    SpeciesAffinity == "evensplit_01" ~ "50% 0, 50% 1",
+    SpeciesAffinity == "runif" ~ "Uniform(0, 1)",
+    TRUE ~ SpeciesAffinity
+  ),
+  SpeciesAffinity = factor(SpeciesAffinity, levels = c(
+    "100% 0", "50% 0, 50% 1", "Uniform(0, 1)"
+  ), ordered = TRUE),
+  Intervention = factor(
+    Intervention,
+    levels = c(
+      "(0)", "(0)->(0.5)", "(0)->(1)",
+      "(0.5)->(0)", "(0.5)", "(0.5)->(1)",
+      "(1)->(0)", "(1)->(0.5)", "(1)"
+    ), ordered = TRUE
+  ),
+  InterventionInitial = dplyr::case_when(
+    Intervention %in% c("(0)", "(0)->(0.5)", "(0)->(1)") ~ "(0)",
+    Intervention %in% c("(0.5)->(0)", "(0.5)", "(0.5)->(1)") ~ "(0.5)",
+    Intervention %in% c("(1)->(0)", "(1)->(0.5)", "(1)") ~ "(1)",
+    TRUE ~ NA_character_
+  ),
+  InterventionInitial = factor(
+    InterventionInitial,
+    levels = c(
+      "(0)", "(0.5)",  "(1)"
+    ), ordered = TRUE
+  ),
+  InterventionFinal = dplyr::case_when(
+    Intervention %in% c("(0)", "(0.5)->(0)", "(1)->(0)") ~ "(0)",
+    Intervention %in% c("(0)->(0.5)", "(0.5)",  "(1)->(0.5)") ~ "(0.5)",
+    Intervention %in% c("(0)->(1)", "(0.5)->(1)", "(1)") ~ "(1)",
+    TRUE ~ NA_character_
+  ),
+  InterventionFinal = factor(
+    InterventionFinal,
+    levels = c(
+      "(0)", "(0.5)",  "(1)"
+    ), ordered = TRUE
+  )
+)
+presTemp <- presTemp %>% dplyr::mutate(
+  SpeciesAffinity = dplyr::case_when(
+    SpeciesAffinity == "rep_0" ~ "100% 0",
+    SpeciesAffinity == "evensplit_01" ~ "50% 0, 50% 1",
+    SpeciesAffinity == "runif" ~ "Uniform(0, 1)",
+    TRUE ~ SpeciesAffinity
+  ),
+  SpeciesAffinity = factor(SpeciesAffinity, levels = c(
+    "100% 0", "50% 0, 50% 1", "Uniform(0, 1)"
+  ), ordered = TRUE),
+  Intervention = factor(
+    Intervention,
+    levels = c(
+      "(0)", "(0)->(0.5)", "(0)->(1)",
+      "(0.5)->(0)", "(0.5)", "(0.5)->(1)",
+      "(1)->(0)", "(1)->(0.5)", "(1)"
+    ), ordered = TRUE
+  ),
+  InterventionInitial = dplyr::case_when(
+    Intervention %in% c("(0)", "(0)->(0.5)", "(0)->(1)") ~ "(0)",
+    Intervention %in% c("(0.5)->(0)", "(0.5)", "(0.5)->(1)") ~ "(0.5)",
+    Intervention %in% c("(1)->(0)", "(1)->(0.5)", "(1)") ~ "(1)",
+    TRUE ~ NA_character_
+  ),
+  InterventionInitial = factor(
+    InterventionInitial,
+    levels = c(
+      "(0)", "(0.5)",  "(1)"
+    ), ordered = TRUE
+  ),
+  InterventionFinal = dplyr::case_when(
+    Intervention %in% c("(0)", "(0.5)->(0)", "(1)->(0)") ~ "(0)",
+    Intervention %in% c("(0)->(0.5)", "(0.5)",  "(1)->(0.5)") ~ "(0.5)",
+    Intervention %in% c("(0)->(1)", "(0.5)->(1)", "(1)") ~ "(1)",
+    TRUE ~ NA_character_
+  ),
+  InterventionFinal = factor(
+    InterventionFinal,
+    levels = c(
+      "(0)", "(0.5)",  "(1)"
+    ), ordered = TRUE
+  )
+)
+
 ## Columns: ###################################################################
 ### Both: #####################################################################
 # "Time": The time of the record of the diversity or presence.
@@ -163,6 +250,224 @@ ImageAffinity <- ggplot2::ggplot(
 ) + ggplot2::theme_minimal(
 )
 
+
+### Abundance Line: ###########################################################
+ImageAbundance2_Geometric <- ggplot2::ggplot(
+  presTemp %>% dplyr::filter(
+    PoolPatchSeed == PoolPatchSeed[1]
+  ) %>% dplyr::group_by(
+    Time, Intervention, SpeciesAffinity
+  ) %>% dplyr::summarise(
+    Abundance = exp(mean(log(Abundance))),
+    .groups = "drop"
+  ),
+  ggplot2::aes(x = Time, y = Abundance, color = Intervention,
+               group = interaction(Intervention))
+) + ggplot2::geom_line(
+) + ggplot2::facet_grid(
+  # Row:
+  # SpeciesAffinity ~ .,#Intervention,# PoolPatchSeed, # + TimeSubset,
+  # Column:
+  . ~ SpeciesAffinity,
+  scales = "free"
+) + ggplot2::scale_color_manual(
+  values = colorPalette, aesthetics = c("color", "fill")
+) + ggplot2::scale_y_log10(
+) + ggplot2::scale_x_continuous(
+  breaks = c(10000, 20000, 30000),
+  minor_breaks = NULL
+) + ggplot2::theme_minimal(
+)
+ImageAbundance2 <- ggplot2::ggplot(
+  presTemp %>% dplyr::filter(
+    PoolPatchSeed == PoolPatchSeed[1]
+  ) %>% dplyr::group_by(
+    Time, Intervention, SpeciesAffinity
+  ) %>% dplyr::summarise(
+    Abundance = mean(Abundance),
+    .groups = "drop"
+  ),
+  ggplot2::aes(x = Time, y = Abundance, color = Intervention,
+               group = interaction(Intervention))
+) + ggplot2::geom_line(
+) + ggplot2::facet_grid(
+  # Row:
+  # SpeciesAffinity ~ .,#Intervention,# PoolPatchSeed, # + TimeSubset,
+  # Column:
+  . ~ SpeciesAffinity,
+  scales = "free"
+) + ggplot2::scale_color_manual(
+  values = colorPalette, aesthetics = c("color", "fill")
+) + ggplot2::scale_y_log10(
+) + ggplot2::scale_x_continuous(
+  breaks = c(10000, 20000, 30000),
+  minor_breaks = NULL
+) + ggplot2::theme_minimal(
+)
+
+ImageAbundance3 <- ggplot2::ggplot(
+  presTemp %>% dplyr::filter(
+    PoolPatchSeed == PoolPatchSeed[1]
+  ) %>% dplyr::group_by(
+    Intervention, Species, SpeciesAffinity
+  ) %>% dplyr::mutate(
+    Run = cumsum(c(0, na.omit(Time > lag(Time)+10)))
+  ),
+  ggplot2::aes(x = Time, y = Abundance, color = Intervention,
+               group = interaction(Intervention, Species, Run))
+) + ggplot2::geom_line(
+  alpha = 0.5
+) + ggplot2::facet_grid(
+  # Row:
+  # SpeciesAffinity ~ .,#Intervention,# PoolPatchSeed, # + TimeSubset,
+  # Column:
+  Intervention ~ SpeciesAffinity,
+  scales = "free"
+) + ggplot2::scale_color_manual(
+  values = colorPalette, aesthetics = c("color", "fill")
+) + ggplot2::scale_y_log10(
+) + ggplot2::scale_x_continuous(
+  breaks = c(10000, 20000, 30000),
+  minor_breaks = NULL
+) + ggplot2::theme_minimal(
+)
+
+### Size Line: ################################################################
+ImageSize2_Geometric <- ggplot2::ggplot(
+  presTemp %>% dplyr::filter(
+    PoolPatchSeed == PoolPatchSeed[1]
+  ) %>% dplyr::group_by(
+    Time, Intervention, SpeciesAffinity
+  ) %>% dplyr::summarise(
+    Size = exp(mean(log(Size))),
+    .groups = "drop"
+  ),
+  ggplot2::aes(x = Time, y = Size, color = Intervention,
+               group = interaction(Intervention))
+) + ggplot2::geom_line(
+) + ggplot2::facet_grid(
+  # Row:
+  # SpeciesAffinity ~ .,#Intervention,# PoolPatchSeed, # + TimeSubset,
+  # Column:
+  . ~ SpeciesAffinity,
+  scales = "free"
+) + ggplot2::scale_color_manual(
+  values = colorPalette, aesthetics = c("color", "fill")
+) + ggplot2::scale_y_log10(
+) + ggplot2::scale_x_continuous(
+  breaks = c(10000, 20000, 30000),
+  minor_breaks = NULL
+) + ggplot2::theme_minimal(
+)
+ImageSize2 <- ggplot2::ggplot(
+  presTemp %>% dplyr::filter(
+    PoolPatchSeed == PoolPatchSeed[1]
+  ) %>% dplyr::group_by(
+    Time, Intervention, SpeciesAffinity
+  ) %>% dplyr::summarise(
+    Size = mean(Size),
+    .groups = "drop"
+  ),
+  ggplot2::aes(x = Time, y = Size, color = Intervention,
+               group = interaction(Intervention))
+) + ggplot2::geom_line(
+) + ggplot2::facet_grid(
+  # Row:
+  # SpeciesAffinity ~ .,#Intervention,# PoolPatchSeed, # + TimeSubset,
+  # Column:
+  . ~ SpeciesAffinity,
+  scales = "free"
+) + ggplot2::scale_color_manual(
+  values = colorPalette, aesthetics = c("color", "fill")
+) + ggplot2::scale_y_log10(
+) + ggplot2::scale_x_continuous(
+  breaks = c(10000, 20000, 30000),
+  minor_breaks = NULL
+) + ggplot2::theme_minimal(
+)
+
+ImageSize3 <- ggplot2::ggplot(
+  presTemp %>% dplyr::filter(
+    PoolPatchSeed == PoolPatchSeed[1]
+  ) %>% dplyr::group_by(
+    Intervention, Species, SpeciesAffinity
+  ) %>% dplyr::mutate(
+    Run = cumsum(c(0, na.omit(Time > lag(Time)+10)))
+  ),
+  ggplot2::aes(x = Time, y = Size, color = Affinity,
+               group = interaction(Intervention, Species, Run))
+) + ggplot2::geom_line(
+  alpha = 0.5
+) + ggplot2::facet_grid(
+  # Row:
+  # SpeciesAffinity ~ .,#Intervention,# PoolPatchSeed, # + TimeSubset,
+  # Column:
+  Intervention ~ SpeciesAffinity,
+  scales = "free"
+# ) + ggplot2::scale_color_manual(
+  # values = colorPalette, aesthetics = c("color", "fill")
+) + ggplot2::scale_y_log10(
+) + ggplot2::scale_x_continuous(
+  breaks = c(10000, 20000, 30000),
+  minor_breaks = NULL
+) + ggplot2::theme_minimal(
+)
+
+### Affinity Line: ############################################################
+
+ImageAffinity2 <- ggplot2::ggplot(
+  presTemp %>% dplyr::filter(
+    PoolPatchSeed == PoolPatchSeed[1]
+  ) %>% dplyr::group_by(
+    Time, Intervention, SpeciesAffinity
+  ) %>% dplyr::summarise(
+    Affinity = mean(Affinity),
+    .groups = "drop"
+  ),
+  ggplot2::aes(x = Time, y = Affinity, color = Intervention,
+               group = interaction(Intervention))
+) + ggplot2::geom_line(
+) + ggplot2::facet_grid(
+  # Row:
+  # SpeciesAffinity ~ .,#Intervention,# PoolPatchSeed, # + TimeSubset,
+  # Column:
+  . ~ SpeciesAffinity,
+  scales = "free"
+) + ggplot2::scale_color_manual(
+  values = colorPalette, aesthetics = c("color", "fill")
+) + ggplot2::scale_x_continuous(
+  breaks = c(10000, 20000, 30000),
+  minor_breaks = NULL
+) + ggplot2::theme_minimal(
+)
+
+ImageAffinity3 <- ggplot2::ggplot(
+  presTemp %>% dplyr::filter(
+    PoolPatchSeed == PoolPatchSeed[1]
+  ) %>% dplyr::group_by(
+    Intervention, Species, SpeciesAffinity
+  ) %>% dplyr::mutate(
+    Run = cumsum(c(0, na.omit(Time > lag(Time)+10)))
+  ),
+  ggplot2::aes(x = Time, y = Affinity, color = Intervention,
+               group = interaction(Intervention, Species, Run))
+) + ggplot2::geom_line(
+  alpha = 0.5
+) + ggplot2::facet_grid(
+  # Row:
+  # SpeciesAffinity ~ .,#Intervention,# PoolPatchSeed, # + TimeSubset,
+  # Column:
+  Intervention ~ SpeciesAffinity,
+  scales = "free"
+) + ggplot2::scale_color_manual(
+  values = colorPalette, aesthetics = c("color", "fill")
+) + ggplot2::scale_x_continuous(
+  breaks = c(10000, 20000, 30000),
+  minor_breaks = NULL
+) + ggplot2::theme_minimal(
+)
+
+
 ## Combine: ###################################################################
 # Vertical arrangement
 ggpubr::ggarrange(
@@ -182,3 +487,22 @@ ggpubr::ggarrange(
   # Common legend, but note that counts are not carried over.
   # Align forces the plotting areas to match (across the column).
   common.legend = TRUE, legend = "right", ncol = 1, align = "v")
+
+ggpubr::ggarrange(
+  # Richness, no x description.
+  ImageRichness + ggplot2::theme(axis.title.x = ggplot2::element_blank()),
+  # Abundance, no x description and no facet labels.
+  ImageAbundance2_Geometric + ggplot2::theme(axis.title.x = ggplot2::element_blank(),
+                                  strip.background = ggplot2::element_blank(),
+                                  strip.text = ggplot2::element_blank()),
+  # Size, no x description and no facet labels.
+  ImageSize2_Geometric + ggplot2::theme(axis.title.x = ggplot2::element_blank(),
+                             strip.background = ggplot2::element_blank(),
+                             strip.text = ggplot2::element_blank()),
+  # Affinity, no facet labels.
+  ImageAffinity2 + ggplot2::theme(strip.background = ggplot2::element_blank(),
+                                 strip.text = ggplot2::element_blank()),
+  # Common legend, but note that counts are not carried over.
+  # Align forces the plotting areas to match (across the column).
+  common.legend = TRUE, legend = "right", ncol = 1, align = "v")
+
