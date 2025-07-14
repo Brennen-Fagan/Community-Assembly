da9cc9ce4d1ab5454f2a470cd7a54228c9be1ac2
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2025-07-14 12:29:31 +0100

    Update TimeSpaceAndTimeSeries-9h2-CombinedPlots.R
    A, hopefully punchier, figure 2 in which I am trying to implicitly convey the causality in the system using similar plots, axes, and backgrounds.

--- experiments/VariantExperiments/TimeSpaceAndTimeSeries-9h2-CombinedPlots.R
+++ experiments/VariantExperiments/TimeSpaceAndTimeSeries-9h2-CombinedPlots.R
@@ -187,7 +187,7 @@ plotMeanAndInner <- function(
     facets
   ) + ggplot2::scale_color_manual(
     values = colorPalette, aesthetics = c("color", "fill"),
-    name = "Habitat Land-use"
+    name = "Local Land-use"
   ) + ggplot2::scale_linetype_manual(
     name = "Species Preferences",
     values = linetypePalette
@@ -518,3 +518,225 @@ targetEnvsIndex <- do.call(
 targetEnvsIndex <- cbind(ID = 1:nrow(targetEnvsIndex), targetEnvsIndex)
 
 # Main Plots: #################################################################
+### Plot 2:####################################################################
+# a=>b&c
+newplot2_dataA <- diversitiesRichness %>% tidytable::filter(
+  SpeciesAffinity == "100% 0",
+  NicheDistance == "5",
+  Intervention %in% c("(0)", "(0.25)", "(0.5)", "(0.75)", "(1)"),
+  (PoolPatchSeed %in% as.character(343:386)),
+  Metric == "Alpha Hill:0",
+  is.na(Subset)
+) %>% tidytable::left_join(endTimes %>% dplyr::select(-Times))
+
+newplot2_indices <- targetEnvsIndex %>% tidytable::filter(
+  SpeciesAffinity == "100% 0",
+  NicheDistance == "5",
+  Intervention %in% c("(0)", "(0.5)", "(1)"),
+  (PoolPatchSeed %in% as.character(343:386))
+)
+
+newplot2_dataC <- Pers %>% tidytable::filter(
+  SpeciesAffinity == "100% 0",
+  NicheDistance == "5",
+  Intervention %in% c("(0)", "(0.25)", "(0.5)", "(0.75)", "(1)"),
+  (PoolPatchSeed %in% as.character(343:386))
+) %>% tidytable::filter(
+  In < Stop, Out > Start # Not things outside of [Start, Stop]
+) %>% tidytable::mutate(
+  # Shorten intervals for equivalent comparisons.
+  InType = ifelse(In < Start, "Persistent", InType),
+  OutType = ifelse(Out > Stop, "Persistent", OutType),
+  In = ifelse(In < Start, Start, In),
+  Out = ifelse(Out > Stop, Stop, Out),
+  Persistence = Out - In
+) %>% tidytable::group_by(
+  Species, Environment, SpeciesType, Size, ReproductionRate, Speed, AffinityBins,
+  PoolPatch, PoolPatchSeed, Interactions, InteractionsSeed, Events, EventsSeed,
+  InitialConditions, InitialConditionsSeed, DispersalParam, NicheDistance,
+  Affinity, AffinitySeed, InterventionPatchType, InterventionPatchSeed,
+  InterventionTimeType, InterventionTimeSeed, InterventionDispersal,
+  InterventionNicheDistance, Intervention, SpeciesAffinity, Start, Stop
+) %>% tidytable::summarise( # Sum over Appearances.
+  Persistence = sum(Persistence),
+  .groups = "drop"
+)
+
+##### a: ######################################################################
+newplot2_a <- plotMeanAndInner(
+  newplot2_dataA %>% tidytable::filter(
+    Intervention %in% c("(0)", "(0.5)", "(1)")
+  ), CIs = 0.75, facets = as.formula(. ~ .)
+) + ggplot2::geom_point(
+  data = newplot2_dataA %>% tidytable::filter(
+    PoolPatchSeed == targetSeed,
+    Intervention %in% c("(0)", "(0.5)", "(1)"),
+    abs(Time - targetTimes) == min(abs(Time - targetTimes))
+  )
+) + ggplot2::labs(
+  y = "Richness"
+) + ggplot2::guides(
+  linetype = "none",
+  color = ggplot2::guide_legend(ncol = 3),
+  fill = ggplot2::guide_legend(ncol = 3)
+) + ggplot2::coord_cartesian(
+  xlim = c(0, 40000), ylim = c(0, 42), expand = FALSE
+) + ggplot2::annotation_custom(
+  ggplot2::ggplotGrob(
+    targetEnvs[[
+      newplot2_indices$ID[1]
+      ]]$singletonGraphs[[1]][[1]] + ggplot2::theme_void(
+      ) + ggplot2::theme(
+        plot.background = ggplot2::element_rect(fill = "white")
+      )
+  ),
+  xmin = 30500, xmax = 40000, ymin = 7, ymax = 17
+) + ggplot2::annotation_custom(
+  ggplot2::ggplotGrob(
+    targetEnvs[[
+      newplot2_indices$ID[2]
+      ]]$singletonGraphs[[1]][[1]] + ggplot2::theme_void(
+      ) + ggplot2::theme(
+        plot.background = ggplot2::element_rect(fill = "white")
+      )
+  ),
+  xmin = 30500, xmax = 40000, ymin = 18, ymax = 28
+) + ggplot2::annotation_custom(
+  ggplot2::ggplotGrob(
+    targetEnvs[[
+      newplot2_indices$ID[3]
+      ]]$singletonGraphs[[1]][[1]] + ggplot2::theme_void(
+      ) + ggplot2::theme(
+        plot.background = ggplot2::element_rect(fill = "white")
+      )
+  ),
+  xmin = 30500, xmax = 40000, ymin = 29, ymax = 39
+) + ggplot2::geom_rect(
+  data = data.frame(
+    1 # 1 rectangle per row, so dummy df to prevent overplotting
+  ),
+  xmin = min(newplot2_dataA$Start),
+  xmax = max(newplot2_dataA$Stop),
+  ymin = 0, ymax = max(newplot2_dataA$Value),
+  fill = "grey",
+  alpha = 0.2,
+  inherit.aes = FALSE
+) + ggplot2::labs(
+  tag = "a)"
+) + ggplot2::theme(
+  legend.position = c(0.3, 0.09),
+  plot.tag.position = c(0.025, 1)
+) + ggplot2::scale_x_continuous(
+  breaks = (0:3)*10000
+)
+
+##### b: ######################################################################
+newplot2_b <- ggplot2::ggplot(
+  newplot2_dataA %>% tidytable::filter(
+    Time > Start, Time < Stop
+  ) %>% tidytable::group_by(
+    PoolPatchSeed, Intervention, SpeciesAffinity
+  ) %>% tidytable::summarise(
+    Value = mean(Value)
+  ),
+  ggplot2::aes(
+    x = Intervention,
+    y = Value,
+    color = Intervention
+  )
+) + ggplot2::geom_rect(
+  data = data.frame(
+    1 # 1 rectangle per row, so dummy df to prevent overplotting
+  ),
+  xmin = 0,
+  xmax = 6,
+  ymin = 0, ymax = max(newplot2_dataA$Value),
+  fill = "grey",
+  alpha = 0.2,
+  inherit.aes = FALSE
+) + ggplot2::geom_violin(
+  position = ggplot2::position_dodge(0.9)
+) + ggplot2::geom_boxplot(
+  notch = TRUE, outlier.size = 0,
+  position = ggplot2::position_dodge(0.9),
+  width = 0.28
+) + ggplot2::geom_jitter(
+  alpha = 0.25
+) + ggplot2::scale_color_manual(
+  values = colorPalette, aesthetics = c("color", "fill"),
+  name = "Local Land-use"
+) + ggplot2::labs(
+  tag = "b)"
+) + ggplot2::theme_minimal(
+) + ggplot2::theme(
+  plot.tag.position = c(0.05, 1)
+) + ggplot2::labs(
+  y = "Avg. Richness",
+  x = "Local Land-use"
+) + ggplot2::guides(
+  color = "none",
+  fill = "none"
+) + ggplot2::coord_cartesian(
+  ylim = c(0, 42), expand = FALSE
+) + ggplot2::annotate(
+  "text", x = c(1.5, 4.5), y = 5, label = c("High\nMatch", "Low\nMatch")
+)
+
+##### c: ######################################################################
+newplot2_c <- ggplot2::ggplot(
+  newplot2_dataC,
+  ggplot2::aes(
+    y = Persistence,
+    x = Intervention,
+    color = Intervention,
+    group = interaction(Intervention, SpeciesType),
+    fill = SpeciesType
+  )
+) + ggplot2::geom_rect(
+  data = data.frame(
+    1 # 1 rectangle per row, so dummy df to prevent overplotting
+  ),
+  xmin = 0,
+  xmax = 6,
+  ymin = -Inf, ymax = Inf,
+  fill = "grey",
+  alpha = 0.2,
+  inherit.aes = FALSE
+) + ggplot2::geom_violin(
+  position = ggplot2::position_dodge(0.9), show.legend = FALSE
+) + ggplot2::geom_boxplot(
+  notch = TRUE, outlier.size = 0,
+  position = ggplot2::position_dodge(0.9),
+  width = 0.28, show.legend = FALSE,
+  color = "black"
+) + ggplot2::scale_color_manual(
+  values = colorPalette,
+  name = "Habitat Land-use"
+) + ggplot2::scale_fill_manual(
+  values = c("limegreen", "goldenrod2")
+) + ggplot2::scale_y_log10(
+) + ggplot2::theme_minimal(
+) + ggplot2::labs(
+  tag = "c)", x = "Habitat"
+) + ggplot2::theme(
+  plot.tag.position = c(0.05, 1)
+) + ggplot2::facet_grid(
+  SpeciesType ~ .
+) + ggplot2::scale_x_discrete(
+  breaks = c("(0)", "(0.5)", "(1)")
+) + ggplot2::labs(
+  x = "Local Land-use"
+)
+
+newplot2 <- ggpubr::ggarrange(
+  plotlist = list(
+    newplot2_a,
+    newplot2_b,
+    newplot2_c
+  ), nrow = 1, widths = c(0.5, 0.27, 0.23)
+)
+
+
+ggplot2::ggsave(plot = newplot2, filename = "Figure2_Prototype6.png",
+                units = "cm", width = 6.5*3, height = 6.5*2)
+
