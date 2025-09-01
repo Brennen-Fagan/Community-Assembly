b501354c80168b1ef02ba8baf2eff8f53aad4e2e
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2025-09-01 12:50:44 +0100

    Update TimeSpaceAndTimeSeries-10j-Supplement11.R
    S11 is now in a good enough state as well! Not much specifically to say here as the code should speak for itself. We may need to break this down by our various subsets (species type and adaptation) though.

--- experiments/FaganEtAl2025/TimeSpaceAndTimeSeries-10j-Supplement11.R
+++ experiments/FaganEtAl2025/TimeSpaceAndTimeSeries-10j-Supplement11.R
@@ -1,8 +1,13 @@
 # Setup: ######################################################################
+# Capture the ways in which species enter and exit the basic set of systems.
+# These are the "in/out" statistics, according to whether there was a
+# successful/failed immigration attempt, a neutral stochastic extirpation, or an
+# extirpation due to species dynamics. We keep counts and percentages and see
+# how they vary as the land-use and land-use preferences vary.
 
 source("TimeSpaceAndTimeSeries-10h-PlotPreparations.R")
 source("TimeSpaceAndTimeSeries-10i-PreparationsPersistence.R")
-supplement11<- list()
+supplement11 <- list()
 
 ### 11 Supplement: #############################################################
 ##### In/Out Statistics: ######################################################
@@ -90,33 +95,41 @@ supplement11<- list()
 #   "Average Count in a Simulation"
 # )
 
-rbind(
+supplement11$data <- tidytable::bind_rows(
   Pers |> tidytable::filter(
-    ...,
+    # SpeciesPreferences == "100% 0",
+    NicheDistance == defaultNicheDistance,
+    # Intervention %in% c("(0)", "(0.25)", "(0.5)", "(0.75)", "(1)"),
+    PoolPatchSeed %in% basePoolPatchSeeds,
     Persistence > 0,
     InType != externalNames["Dispersal"],
     In < Stop, Out > Start
   ) |> tidytable::group_by(
     SpeciesPreferences, InType, OutType, Intervention
   ) |> tidytable::summarise(
-    ChartValue = tidytable::n() / tidytable::n_distinct(PoolPatchSeed)
+    Average = tidytable::n() / tidytable::n_distinct(PoolPatchSeed)
   ),
-  ColExt |> tidytable::left_join(
+  ColExt |> tidytable::filter(
+    # Filter what we can:
+    NicheDistance == defaultNicheDistance,
+    PoolPatchSeed %in% basePoolPatchSeeds,
+    !Success | EventType == "Present"
+  ) |> tidytable::left_join(
     # Start and Stop aren't already present in this version
-    endTimes
+    endTimes |> tidytable::select(-Times)
+  ) |> tidytable::filter(
+    Times > Start, Times < Stop
   ) |> tidytable::mutate(
     SpeciesPreferences =
-      affinityDictionaryOrigin$SpeciesAffinities[as.numeric(Affinity)],
-    Intervention = unlist(mapply(
-      FUN = interventionNamingScheme,
-      Affinity, PoolPatch, InterventionPatchType
-    ))
-  ) |> changeAffinityLevels(
-  ) |> changeInterventionLevels(
-  ) |> tidytable::filter(
-    ...,
-    !Success | EventType == "Present",
-    Times > Start, Times < Stop
+      speciesAffinityDictionaryOrigin$SpeciesAffinities[as.numeric(SpeciesAffinity)]
+  ) |> changePreferencesLevels(
+    # ) |> tidytable::filter(
+    #   SpeciesPreferences == "100% 0"
+  ) |> tidytable::left_join(
+    interventionStrings,
+    by = c("PatchAffinity", "PoolPatch", "InterventionPatchType")
+    # ) |> tidytable::filter(
+    #   Intervention %in% c("(0)", "(0.25)", "(0.5)", "(0.75)", "(1)")
   ) |> tidytable::mutate(
     InType = externalNames[
       ifelse(EventType == "Arrival", "Failed Arrival", "Present")
@@ -125,6 +138,46 @@ rbind(
   ) |> tidytable::group_by(
     SpeciesPreferences, InType, OutType, Intervention
   ) |> tidytable::summarise(
-    ChartValue = tidytable::n() / tidytable::n_distinct(PoolPatchSeed)
+    Average = tidytable::n() / tidytable::n_distinct(PoolPatchSeed)
   )
+) |> changeInterventionLevels(
+)
+
+supplement11$labels <- supplement11$data |> tidytable::group_by(
+  # InType, OutType,
+  SpeciesPreferences, InterventionInitial, InterventionFinal
+) |> tidytable::mutate(
+  Percentage = Average / sum(Average), # /44's cancel out.
+  Text = paste0(formatC(Percentage*100, digits = 1, format = "f"), "%"),
+  y = 400
+)
+
+supplement11$plot <- ggplot2::ggplot(
+  supplement11$data,
+  ggplot2::aes(x = interaction(InType,
+                               OutType,
+                               sep = "\n", lex.order = TRUE),#"",
+               y = Average,
+               fill = interaction(InType,
+                                  OutType,
+                                  sep = "/"))
+) + ggplot2::geom_col(
+  show.legend = FALSE
+) + ggplot2::geom_text(
+  data = supplement11$labels,
+  mapping = ggplot2::aes(y = y, label = Text),
+  show.legend = FALSE
+) + ggplot2::facet_grid(
+  SpeciesPreferences + InterventionInitial ~ InterventionFinal
+) + ggplot2::theme_minimal(
+) + ggplot2::labs(
+  subtitle =
+    "Columns = Final Land-use, Rows = Initial Land-use and Species Type",
+  x = "Enter/Exit Type", y = "Average Count"#, fill = "In/Out"
+)
+
+ggplot2::ggsave(
+  supplement11$plot,
+  filename = "Figure_supplement11_v1.png",
+  units = "cm", width = 20*3, height = 20*2
 )
