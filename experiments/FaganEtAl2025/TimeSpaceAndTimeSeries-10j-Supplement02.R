03158aff255ed2ef7d2249ee1591c273f0dc5d4b
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2025-09-09 09:30:27 +0100

    Update TimeSpaceAndTimeSeries-10j-Supplement02.R
    Adding an abundance plot to Supplement 2. Note it is of Average Total Abundance (not Average [Average Abundance]).

--- experiments/FaganEtAl2025/TimeSpaceAndTimeSeries-10j-Supplement02.R
+++ experiments/FaganEtAl2025/TimeSpaceAndTimeSeries-10j-Supplement02.R
@@ -3,6 +3,7 @@
 
 source("TimeSpaceAndTimeSeries-10h-PlotPreparations.R")
 source("TimeSpaceAndTimeSeries-10i-PreparationsRichness.R")
+source("TimeSpaceAndTimeSeries-10i-PreparationsAbund.R")
 
 supplement2 <- list()
 
@@ -13,6 +14,15 @@ supplement2$dataA <- diversitiesRichness |> tidytable::filter(
   Intervention %in% c("(0)", "(0.25)", "(0.5)", "(0.75)", "(1)"),
   PoolPatchSeed %in% basePoolPatchSeeds,
   Metric == "Alpha Hill:0",
+  !is.na(Subset)
+)
+
+supplement2$dataB <- diversitiesAbund |> tidytable::filter(
+  SpeciesPreferences == "100% 0",
+  NicheDistance == defaultNicheDistance,
+  Intervention %in% c("(0)", "(0.25)", "(0.5)", "(0.75)", "(1)"),
+  PoolPatchSeed %in% basePoolPatchSeeds,
+  Metric == "Alpha Abundance",
   !is.na(Subset)
 )
 
@@ -117,12 +127,67 @@ supplement2$plotB <- ggplot2::ggplot(
          labels = c("Consumer", "Basal"), ordered = TRUE) ~ .
 )
 
+##### c: ######################################################################
+supplement2$plotC <- ggplot2::ggplot(
+  supplement2$dataB |> tidytable::filter(
+    Time > Start, Time < Stop
+  ) |> tidytable::group_by(
+    PoolPatchSeed, Intervention, SpeciesPreferences, Subset
+  ) |> tidytable::summarise(
+    Value = mean(Value)
+  ),
+  ggplot2::aes(
+    x = Intervention,
+    y = Value,
+    color = Intervention
+  )
+) + ggplot2::geom_rect(
+  data = data.frame(
+    1 # 1 rectangle per row, so dummy df to prevent overplotting
+  ),
+  xmin = 0,
+  xmax = 6,
+  ymin = 0, ymax = max(supplement2$dataB$Value),
+  fill = "grey",
+  alpha = 0.2,
+  inherit.aes = FALSE
+) + ggplot2::geom_violin(
+  position = ggplot2::position_dodge(0.9)
+) + ggplot2::geom_boxplot(
+  notch = TRUE, outlier.size = 0,
+  position = ggplot2::position_dodge(0.9),
+  width = 0.28, alpha = 0.5
+  # ) + ggplot2::geom_jitter(
+  #   alpha = 0.25
+) + ggplot2::scale_color_manual(
+  values = colorPalette, aesthetics = c("color", "fill"),
+  name = "Habitat's Land-use"
+) + ggplot2::labs(
+  tag = "c)"
+) + ggplot2::theme_minimal(
+) + ggplot2::theme(
+  plot.tag.position = c(0.05, 1)
+) + ggplot2::labs(
+  y = "Avg. Total Abundance",
+  x = "Habitat's Land-use"
+) + ggplot2::guides(
+  color = "none",
+  fill = "none"
+  # ) + ggplot2::annotate(
+  #   "text", x = c(1.5, 4.5), y = 5, label = c("Well\nAdapted", "Poorly\nAdapted")
+) + ggplot2::facet_grid(
+  factor(Subset, levels = c("Consumer_0", "Basal_0"),
+         labels = c("Consumer", "Basal"), ordered = TRUE) ~ .,
+  scales = "free_y"
+)
+
 supplement2$plot <- ggpubr::ggarrange(
   plotlist = list(
     supplement2$plotA,
-    supplement2$plotB
-  ), nrow = 1, widths = c(0.5, 0.4)
+    supplement2$plotB,
+    supplement2$plotC
+  ), nrow = 1, widths = c(0.5, 0.4, 0.4)
 )
 
-ggplot2::ggsave(plot = supplement2$plot, filename = "Figure_supplement2_v1.pdf",
-                units = "cm", width = 6.5*3, height = 6.5*2)
+ggplot2::ggsave(plot = supplement2$plot, filename = "Figure_supplement2_v2.pdf",
+                units = "cm", width = 6.5*4, height = 6.5*2)
