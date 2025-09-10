0a38e319a9c99492d42761c57865fc982922c822
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2025-09-10 12:00:23 +0100

    Update TimeSpaceAndTimeSeries-10j-Supplement02.R
    Asked for Average Average Abundance rather than Average Total Abundance.

--- experiments/FaganEtAl2025/TimeSpaceAndTimeSeries-10j-Supplement02.R
+++ experiments/FaganEtAl2025/TimeSpaceAndTimeSeries-10j-Supplement02.R
@@ -26,6 +26,23 @@ supplement2$dataB <- diversitiesAbund |> tidytable::filter(
   !is.na(Subset)
 )
 
+supplement2$dataC <- with(supplement2, {
+  # Average Abundance of species in the patch
+  rbind(dataA, dataB) |> tidytable::filter(
+    Start <= Time, Time <= Stop
+  ) |> tidytable::pivot_wider(
+    names_from = Metric, values_from = Value
+  ) |> tidytable::mutate(
+    `Average Abundance` =
+      ifelse(`Alpha Hill:0` > 0, `Alpha Abundance` / `Alpha Hill:0`, 0)
+  ) |> tidytable::group_by(
+    # Average over relevant times
+    Environment1:DispersalParam
+  ) |> tidytable::summarise(
+    `Average Abundance` = mean(`Average Abundance`)
+  )
+}) # 44 replicates, 5 treatments, 2 subsets.
+
 ##### a: ######################################################################
 supplement2$plotA <- plotMeanAndInner(
   rbind(
@@ -129,28 +146,33 @@ supplement2$plotB <- ggplot2::ggplot(
 
 ##### c: ######################################################################
 supplement2$plotC <- ggplot2::ggplot(
-  supplement2$dataB |> tidytable::filter(
-    Time > Start, Time < Stop
-  ) |> tidytable::group_by(
-    PoolPatchSeed, Intervention, SpeciesPreferences, Subset
-  ) |> tidytable::summarise(
-    Value = mean(Value)
-  ),
+  # supplement2$dataB |> tidytable::filter(
+  #   Time > Start, Time < Stop
+  # ) |> tidytable::group_by(
+  #   PoolPatchSeed, Intervention, SpeciesPreferences, Subset
+  # ) |> tidytable::summarise(
+  #   Value = mean(Value)
+  # ),
+  supplement2$dataC,
   ggplot2::aes(
     x = Intervention,
-    y = Value,
+    # y = Value,
+    y = `Average Abundance`,
     color = Intervention
   )
-) + ggplot2::geom_rect(
-  data = data.frame(
-    1 # 1 rectangle per row, so dummy df to prevent overplotting
-  ),
-  xmin = 0,
-  xmax = 6,
-  ymin = 0, ymax = max(supplement2$dataB$Value),
-  fill = "grey",
-  alpha = 0.2,
-  inherit.aes = FALSE
+# ) + ggplot2::geom_rect(
+#   data = data.frame(
+#     1 # 1 rectangle per row, so dummy df to prevent overplotting
+#   ),
+#   xmin = 0,
+#   xmax = 6,
+#   # ymin = 0,
+#   # ymax = max(supplement2$dataB$Value),
+#   ymin = 0.01,
+#   ymax = 4000,
+#   fill = "grey",
+#   alpha = 0.2,
+#   inherit.aes = FALSE
 ) + ggplot2::geom_violin(
   position = ggplot2::position_dodge(0.9)
 ) + ggplot2::geom_boxplot(
@@ -166,9 +188,12 @@ supplement2$plotC <- ggplot2::ggplot(
   tag = "c)"
 ) + ggplot2::theme_minimal(
 ) + ggplot2::theme(
-  plot.tag.position = c(0.05, 1)
+  plot.tag.position = c(0.05, 1),
+  panel.background = ggplot2::element_rect(
+    fill = "grey90", color = NA
+  )
 ) + ggplot2::labs(
-  y = "Avg. Total Abundance",
+  # y = "Avg. Total Abundance",
   x = "Habitat's Land-use"
 ) + ggplot2::guides(
   color = "none",
@@ -179,6 +204,7 @@ supplement2$plotC <- ggplot2::ggplot(
   factor(Subset, levels = c("Consumer_0", "Basal_0"),
          labels = c("Consumer", "Basal"), ordered = TRUE) ~ .,
   scales = "free_y"
+) + ggplot2::scale_y_log10(
 )
 
 supplement2$plot <- ggpubr::ggarrange(
@@ -189,5 +215,5 @@ supplement2$plot <- ggpubr::ggarrange(
   ), nrow = 1, widths = c(0.5, 0.4, 0.4)
 )
 
-ggplot2::ggsave(plot = supplement2$plot, filename = "Figure_supplement2_v2.pdf",
+ggplot2::ggsave(plot = supplement2$plot, filename = "Figure_supplement2_v3.pdf",
                 units = "cm", width = 6.5*4, height = 6.5*2)
