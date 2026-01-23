67a2887d432b950ff40c5e6ae30a632b7663027a
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2026-01-23 14:32:35 +0000

    F4a and layout for remainder.

--- experiments/FaganEtAl2025/TimeSpaceAndTimeSeries-10j-Figure4.R
+++ experiments/FaganEtAl2025/TimeSpaceAndTimeSeries-10j-Figure4.R
@@ -13,7 +13,121 @@ source("TimeSpaceAndTimeSeries-10i-PreparationsAbund.R")
 
 # This is better as an environment, but that's more opaque.
 figure4 <- list(
-  pref = "100% 0"#"Uniform(0, 1)"
+  pref = "100% 0", #"Uniform(0, 1)"
+  luinitl = "(0.5)", # Land Use INITiaL
+  lufinal = c("(0)", "(0.5)", "(1)") # Land Use FINAL
 )
+
+figure4$prefstring <- switch(
+  figure4$pref,
+  "100% 0" = "", # Base Case
+  "50% 0, 50% 1" = "_5050",
+  "Uniform(0, 1)" = "_Unif"
+)
+figure4$lustring <- switch(
+  figure4$luinitl,
+  "(0)" = "0",
+  "(0.5)" = "", # Base Case
+  "(1)" = "1"
+)
+
+# Main Plots: #################################################################
+### Plot 4: ###################################################################
+##### Data: ###################################################################
+
+# Interventions store the time right before intervention, then the
+# time of the intervention itself. Retrieve this second time.
+# Note it is per-simulation (timescale set by PoolPatch effectively).
+# (If done correctly, the set-up as of 23/01/2026 means there will be
+# round numbers from 1:10, evens from 12:20, and then by 3s 20:50.)
+figure4$interventionTimes <- diversitiesRichness |> tidytable::filter(
+  SpeciesPreferences == "100% 0",
+  NicheDistance == defaultNicheDistance,
+  PoolPatchSeed %in% basePoolPatchSeeds,
+  Metric == "Alpha Hill:0",
+  !is.na(Subset), # Basals and Consumers
+  InterventionInitial != InterventionFinal
+) |> tidytable::select(
+  PoolPatch, PoolPatchSeed, Time
+) |> tidytable::group_by(
+  PoolPatch, PoolPatchSeed
+) |> tidytable::summarise(
+  Time = min(
+    Time[round(Time, 6)!=round(min(Time), 6)]
+  ), # Not min(Time) but the next time.
+  .groups = "drop"
+)
+
+figure4$data <- tidytable::bind_rows(
+  diversitiesRichness |> tidytable::filter(
+    SpeciesPreferences == figure4$pref,
+    NicheDistance == defaultNicheDistance,
+    InterventionInitial == figure4$luinitl,
+    InterventionFinal %in% figure4$lufinal,
+    PoolPatchSeed %in% basePoolPatchSeeds,
+    Metric == "Alpha Hill:0" 
+    # Need all subsets
+  ),
+  diversitiesAbund |> tidytable::filter(
+    SpeciesPreferences == figure4$pref,
+    NicheDistance == defaultNicheDistance,
+    InterventionInitial == figure4$luinitl,
+    InterventionFinal %in% figure4$lufinal,
+    PoolPatchSeed %in% basePoolPatchSeeds,
+    Metric == "Alpha Abundance" 
+    # Need all subsets
+  )
+) |> tidytable::left_join(
+  figure4$interventionTimes |> tidytable::rename(
+    InterventionTime = Time
+  ), 
+  by = c("PoolPatch", "PoolPatchSeed")
+) |> tidytable::mutate(
+  Metric = factor(Metric, levels = c("Alpha Hill:0", "Alpha Abundance"),
+                  labels = c("Richness", "Abundance"), ordered = TRUE),
+  Time = Time - InterventionTime
+) |> tidytable::filter(
+  Time > -1000, Time < 15000
+)
+
+##### a: ######################################################################
+# Summarised time-series plot for overall richness.
+figure4$plotA <- plotMeanAndInner(
+    figure4$data |> tidytable::filter(
+      Metric == "Richness", is.na(Subset)
+    ), CIs = 0.75, facets = as.formula(. ~ .),
+    digits = -2 # Due to temporal resolution of the data, above -2 we can't 
+    # see the trends (not aggregated enough) but at -2, we can't see the
+    # connection (the downward jump, because too aggregated).
+) + ggplot2::labs(
+  x = "Time Since Intervention",
+  y = "Richness"
+) + ggplot2::guides(
+  linetype = "none",
+  color = ggplot2::guide_legend(ncol = 5),
+  fill = ggplot2::guide_legend(ncol = 5)
+) + ggplot2::coord_cartesian(
+  xlim = c(-1000, 15000), ylim = c(0, richnessYMax), expand = FALSE
+) + ggplot2::theme(
+  legend.position = c(0.5, 0.09),
+  plot.tag.position = c(0.025, 0.95),
+  axis.text.x = ggplot2::element_text(hjust = 1)
+)
+
+##### b: ######################################################################
+# HEATMAPS
+
+##### c: ######################################################################
+# Summarised time-series plot for short time scale richness ratio.
+# Note this should be Consumer / Basal (because Consumers sometimes -> 0).
+
+##### d: ######################################################################
+# Summarised time-series plot for short time scale abundance ratio.
+# Note this should be Consumer / Basal (because Consumers sometimes -> 0).
+
+##### SUPPLEMENT: #############################################################
+# Short-long term transition for richness and abundance on log(1+Time) scale
+# expecting to hit the ratios as well as the base values.
+
 
 #4prototype5
