e2e0ef51ad0f4e7174aae9e19fb500d4b93c6a74
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2026-01-27 17:06:41 +0000

    F6 Violin plots

--- experiments/FaganEtAl2025/TimeSpaceAndTimeSeries-10j-Figure6.R
+++ experiments/FaganEtAl2025/TimeSpaceAndTimeSeries-10j-Figure6.R
@@ -23,8 +23,8 @@ figure6$dataRich <- diversitiesRichness |> tidytable::filter(
   PoolPatchSeed %in% basePoolPatchSeeds,
   Metric == "Alpha Hill:0",
   is.na(Subset)
-) |> tidytable::pivot_wider(
-  names_from = Metric, values_from = Value
+)
+
 )
 
 ##### a: ######################################################################
@@ -40,10 +40,8 @@ figure6$plotA <- plotMeanAndInner(
       Intervention %in% c("(0.25)", "(0.75)"),
       abs(Time - 10000) == min(abs(Time - 10000))
     ) |> tidytable::mutate(
-      `Alpha Hill:0` = 10 # coord_cartesian will eliminate these points.
+      Value = 10 # coord_cartesian will eliminate these points.
     )
-  ) |> tidytable::rename(
-    Value = `Alpha Hill:0`
   ), CIs = 0.75, facets = as.formula(SpeciesPreferences ~ .)
 ) + ggplot2::labs(
   y = "Richness"
@@ -59,5 +57,80 @@ figure6$plotA <- plotMeanAndInner(
   breaks = (0:3)*10000
 ) + ggbreak::scale_x_break(
   c(5000, 20000), expand = FALSE
+)
+
+##### b: Violins ##############################################################
+figure6$plotB <- ggplot2::ggplot(
+  figure6$dataRich |> tidytable::filter(
+    Time > Start, Time < Stop, SpeciesPreferences == "50% 0, 50% 1"
+  ) |> tidytable::group_by(
+    PoolPatchSeed, Intervention, SpeciesPreferences
+  ) |> tidytable::summarise(
+    Value = mean(Value)
+  ),
+  ggplot2::aes(
+    x = Intervention,
+    y = Value,
+    color = Intervention
+  )
+) + ggplot2::geom_violin(
+  position = ggplot2::position_dodge(0.9), scale = "count"
+) + ggplot2::geom_boxplot(
+  notch = TRUE, outlier.size = 0,
+  position = ggplot2::position_dodge(0.9),
+  width = 0.28
+) + ggplot2::scale_color_manual(
+  values = colorPalette, aesthetics = c("color", "fill"),
+  name = "Habitat Type"
+) + ggplot2::theme_minimal(
+) + ggplot2::theme(
+  plot.tag.position = c(0.01, 1)
+) + ggplot2::labs(
+  y = "Avg. Richness",
+  x = "Habitat Type"
+) + ggplot2::guides(
+  color = "none",
+  fill = "none"
+) + ggplot2::coord_cartesian(
+  ylim = c(0, richnessYMax), expand = FALSE
+) + ggplot2::facet_wrap(
+  SpeciesPreferences ~ .
+)
+
+figure6$plotC <- ggplot2::ggplot(
+  figure6$dataRich |> tidytable::filter(
+    Time > Start, Time < Stop, SpeciesPreferences == "Uniform(0, 1)"
+  ) |> tidytable::group_by(
+    PoolPatchSeed, Intervention, SpeciesPreferences
+  ) |> tidytable::summarise(
+    Value = mean(Value)
+  ),
+  ggplot2::aes(
+    x = Intervention,
+    y = Value,
+    color = Intervention
+  )
+) + ggplot2::geom_violin(
+  position = ggplot2::position_dodge(0.9), scale = "count"
+) + ggplot2::geom_boxplot(
+  notch = TRUE, outlier.size = 0,
+  position = ggplot2::position_dodge(0.9),
+  width = 0.28
+) + ggplot2::scale_color_manual(
+  values = colorPalette, aesthetics = c("color", "fill"),
+  name = "Habitat Type"
+) + ggplot2::theme_minimal(
+) + ggplot2::theme(
+  plot.tag.position = c(0.01, 1)
+) + ggplot2::labs(
+  y = "Avg. Richness",
+  x = "Habitat Type"
+) + ggplot2::guides(
+  color = "none",
+  fill = "none"
+) + ggplot2::coord_cartesian(
+  ylim = c(0, richnessYMax), expand = FALSE
+) + ggplot2::facet_wrap(
+  SpeciesPreferences ~ .
 )
 
