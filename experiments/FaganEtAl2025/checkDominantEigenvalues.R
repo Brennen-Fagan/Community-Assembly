55a12fe735dadc6853dbf564de1cba00ee9282e4
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2026-02-27 11:14:53 +0000

    Create checkDominantEigenvalues.R
    Built a tool to help retrieve the dominant eigenvalues from the results of a single run. Note that the per capita dynamics need to be specified as well.
    
    To double check before usage in a paper, especially for common errors like off-by-one errors in diffs.

---
+++ experiments/FaganEtAl2025/checkDominantEigenvalues.R
@@ -0,0 +1,53 @@
+checkDominantEigenvalue <- function(x, f) {
+  max(abs(eigen(rootSolve::jacobian.full(x, f), only.values = TRUE)$values))
+}
+
+checkAllDominantEigenvalues <- function(
+  Abundance,
+  Events = NULL,
+  PerCapitaDynamics,
+  DispersalMatrix,
+  MaxChangeSize = 1e-5
+) {
+  # For production version, would need to update to handle t, parms.
+  Dynamics <- function(t, y, parms) {
+    list( # Reaction: PerCapitaDynamics includes interactions and reproduction.
+      as.numeric(
+        y * PerCapitaDynamics(t, y, parms)
+        # Transport: Dispersal means movement of abundance between nodes.
+        + DispersalMatrix %*% y
+      )
+    )
+  }
+  
+  if (!is.null(Events)) {
+    # Look at the times right before a successful event occurs to simplify.
+    CandidateEvents <- Events[Events$Success,]
+    Candidates <- which(Abundance[, 1] %in% CandidateEvents$Times) - 1 # rows
+    if (length(Candidates) != nrow(CandidateEvents)) {
+      warning("Some Candidate Times not found in Events.")
+    }
+  } else {
+    Candidates <- 1:nrow(Abundance) # rows -- not times.
+  }
+  # Check to see if there was much change at the Candidate times.
+  MaxChange <- apply(diff(Abundance[,-1])[Candidates, ], 1, 
+                     function(x) max(abs(x)))
+  Candidates <- Candidates[MaxChange < MaxChangeSize]
+  
+  if (is.null(Events)) {
+    # Take last value in a cluster (shouldn't be clusters if !is.null(events)).
+    Candidates <- Candidates[diff(c(Candidates, Inf)) != 1]
+  }
+  
+  allDominantEigenvalues <- apply(
+    Abundance[Candidates, -1], 1, 
+    function(x) checkDominantEigenvalue(x, Dynamics)
+  )
+  
+  return(data.frame(
+    Time = Abundance[Candidates, 1],
+    Row = Candidates,
+    Eigenvalue = allDominantEigenvalues
+  ))
+}
