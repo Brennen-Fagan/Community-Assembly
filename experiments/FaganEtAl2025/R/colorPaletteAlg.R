f188a7f7b94e496e9dac86be51195b62c2512984
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2026-03-02 12:12:28 +0000

    Push colors away from magenta
    The colors we're presenting are most commonly either the extremes (Cyan, Magenta, Yellow), followed by deviations from Magenta or colors evenly spaced from Cyan and Yellow to Magenta (red, blue). Our red and blue never show up alongside the intervention colors though. So we're pushing the magenta interventions towards cyan and yellow to make them more distinct.

--- experiments/FaganEtAl2025/R/colorPaletteAlg.R
+++ experiments/FaganEtAl2025/R/colorPaletteAlg.R
@@ -1,6 +1,10 @@
+# Base:
 # Algorithmic: first index = 100%, second index = 50%
 #              (0) => 1,0,0, (0.5) => 0,1,0, (1) => 0,0,1
 #              (0.25) => 0.5,0.5,0, (0.75) => 0,0.5,0.5
+
+# Modification:
+# Trying to move colors out from M and towards C and Y.
 colorPaletteAlg <- function(intervention) {
   intervention <- as.numeric(strsplit(
     gsub(intervention, pattern = "[(|)]", replacement = ""),
@@ -11,11 +15,12 @@ colorPaletteAlg <- function(intervention) {
   } else {
     intervention[1]
   }
+
   DescTools::CmykToRgb(
-    min(max(0, (0.5-x)/0.5) + 0.5*max(0, (0.5-y)/0.5), 1),
+    min(1.25*(max(0, (0.5-x)/0.5) + 0.5*max(0, (0.5-y)/0.5)), 1),
     min(max(0, (0.5 - abs(x - 0.5))/0.5)
         + 0.5*max(0, (0.5 - abs(y - 0.5))/0.5), 1),
-    min(max(0, (x-0.5)/0.5)+ 0.5*max(0, (y-0.5)/0.5), 1),
+    min(1.25*(max(0, (x-0.5)/0.5)+ 0.5*max(0, (y-0.5)/0.5)), 1),
     0.25
   )
 }
