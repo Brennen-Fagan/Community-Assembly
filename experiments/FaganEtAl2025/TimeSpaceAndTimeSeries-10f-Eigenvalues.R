229f804196fcf24739517734436b6a837b791ee1
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2026-03-09 15:59:57 +0000

    10f-Eigs -- Chunking for the memory constraint.
    Git seems to have gotten confused with this rearrangement, but it's just moving the old separate functions into a loop together to handle them portion by portion rather than trying to do all of one function then all of the other,

--- experiments/FaganEtAl2025/TimeSpaceAndTimeSeries-10f-Eigenvalues.R
+++ experiments/FaganEtAl2025/TimeSpaceAndTimeSeries-10f-Eigenvalues.R
@@ -139,225 +139,237 @@ poolmats <- lapply(
 allfiles <- dir(datfolders, full.names = TRUE,
                 pattern = "(Simulation|Result|Intervention)")
 
-EigenData <- foreach::foreach(
-  id = iterators::iter(1:length(allfiles)),
-  x = iterators::iter(allfiles)
-  # id = iterators::iter(c(1:100)),
-  # x = iterators::iter(sample(allfiles, 100))
-  #, .packages = c("dplyr", "RMTRCode2")
-) %op% {
-  directory <- '.'
-  librarypath <- file.path(directory, "Rlibs")
-  .libPaths(c(librarypath, .libPaths()))
-  library(RMTRCode2); library(dplyr)
+# Improve memory management by chunking to enable gc's.
+EigenData <- NULL
+nchunks <- 20
+chunks <- round(seq(1, length(allfiles)+1, length.out = nchunks+1))
+for (chunk in 1:nchunks) {
+  print(chunk)
+  EigenData <- foreach::foreach(
+    id = iterators::iter(chunks[chunk]:(chunks[chunk+1]-1)),
+    x = iterators::iter(allfiles[chunks[chunk]:(chunks[chunk+1]-1)])
+    # id = iterators::iter(c(1:100)),
+    # x = iterators::iter(sample(allfiles, 100))
+    #, .packages = c("dplyr", "RMTRCode2")
+  ) %op% {
+    directory <- '.'
+    librarypath <- file.path(directory, "Rlibs")
+    .libPaths(c(librarypath, .libPaths()))
+    library(RMTRCode2); library(dplyr)
 
-  x_properties <- strsplit(basename(x), split = splitchar)
-  stopifnot(length(x_properties) == 1)
+    x_properties <- strsplit(basename(x), split = splitchar)
+    stopifnot(length(x_properties) == 1)
 
-  filename <- file.path(
-    dirname(x),
-    if (flag == "TSTS") {
-      paste0(c(x_properties[[1]][1],
-               "Eigen",
-               x_properties[[1]][3:length(x_properties[[1]])]), collapse = "_")
-    } else if (flag == "Data") {
-      paste0("TSTS_Eigen_",
-             paste0(x_properties[[1]][5:length(x_properties[[1]])],
-                    collapse = "_"))
-    } else {
-      paste0("Eigen_", x)
-    }
-  )
-
-  if(!overwrite && file.exists(filename)) {
-    if (alsoload) {
-      load(filename)
-    }
-  } else {
-    print(paste(id, filename))
-    x_dir <- dirname(x)
-    x_poolind <- which(unlist(lapply(poolmats, function(y) y$Dir == x_dir)))
-    if(any(x_poolind)) {
-      # x_pool <- poolmats[[x_poolind]]$Pool
-      DynamicsFunction <- poolmats[[x_poolind]]$DynamicsFunction
-      InteractionMatrices <- poolmats[[x_poolind]]$InteractionMatrices
-    } else {
-      warning(paste("Pool not found, skipping", id, filename)); next()
-    }
-
-    # Load result to analyse.
-    loaded <- load(x) # names
-    stopifnot(length(loaded) == 1)
-    loaded <- (get(loaded)) # objects
-    # Note: after patch effects, rather than before
-    if ("EffectiveReproductionRate" %in% names(loaded$Ellipsis$Affinity)) {
-      rprime <- loaded$Ellipsis$Affinity$EffectiveReproductionRate
-    } else if ("EffectiveReproductionRateIntervention" %in% names(loaded$Ellipsis$Affinity)) {
-      rprime <- loaded$Ellipsis$Affinity$EffectiveReproductionRateIntervention
-    } else {
-      warning("Reproduction Rate rprime not found."); next()
-    }
-
-    # Different handling is needed depending on if arguments are functions.
-    # (Could functionalise the calls, but that introduces overhead on each call.)
-    if (is.function(rprime)) {
-      # Calculate rprime using Parms$Patch (i.e., within patch)
-      if (is.function(InteractionMatrices$Mats[[1]])) {
-        # Calculate and combine interaction matrices on the fly.
-        PerCapitaDynamics <- DynamicsFunction(
-          rprime,
-          function(t, y, parms) {
-            Matrix::bdiag(lapply(
-              InteractionMatrices$Mats,
-              function(matfunc) {matfunc(t, y, parms)}
-            ))
-          },
-          loaded$NumEnvironments
-        )
+    filename <- file.path(
+      dirname(x),
+      if (flag == "TSTS") {
+        paste0(c(x_properties[[1]][1],
+                 "Eigen",
+                 x_properties[[1]][3:length(x_properties[[1]])]), collapse = "_")
+      } else if (flag == "Data") {
+        paste0("TSTS_Eigen_",
+               paste0(x_properties[[1]][5:length(x_properties[[1]])],
+                      collapse = "_"))
+      } else {
+        paste0("Eigen_", x)
       }
-      else {
-        # Just combine the interaction matrices.
-        PerCapitaDynamics <- DynamicsFunction(
-          rprime,
-          Matrix::bdiag(InteractionMatrices$Mats),
-          loaded$NumEnvironments
-        )
-      }
-    } else {
-      # Treat rprime as constant and explicitly calculated.
-      if (is.function(InteractionMatrices$Mats[[1]])) {
-        # Calculate and combine interaction matrices on the fly.
-        PerCapitaDynamics <- DynamicsFunction(
-          rprime,
-          function(t, y, parms) {
-            Matrix::bdiag(lapply(
-              InteractionMatrices$Mats,
-              function(matfunc) {matfunc(t, y, parms)}
-            ))
-          }
-        )
-      }
-      else {
-        # Just combine the interaction matrices.
-        PerCapitaDynamics <- DynamicsFunction(
-          rprime,
-          Matrix::bdiag(InteractionMatrices$Mats)
-        )
-      }
-    }
-
-    EigData <- list(
-      EigenData = with(loaded, checkAllDominantEigenvalues(
-        Abundance = Abundance,                            # Inside loaded
-        Events = Events,                                  # Inside loaded
-        PerCapitaDynamics = PerCapitaDynamics,            # Assembled from poolmats
-        DispersalMatrix = Parameters$DispersalMatrix,     # Inside loaded
-        MaxChangeSize = 1e-6 # How much change is "too much" to be @ f.p.
-      )),
-      EigenInteraction = max(abs(unlist(lapply(
-        poolmats[[x_poolind]]$InteractionMatrices$Mats,
-        eigen, only.values = TRUE
-      )))),
-      CharacteristicRate = 1/loaded$ReactionTime, # Should match EigenInteraction.
-      ReproductionRate = poolmats[[x_poolind]]$Pool$ReproductionRate, # orig rs
-      Ellipsis = loaded$Ellipsis
     )
 
-    if ("ParentRun" %in% names(EigData$Ellipsis))
-      EigData$Ellipsis$GrandparentRun <- EigData$Ellipsis$ParentRun
-    EigData$Ellipsis$ParentRun <- x
+    if(!overwrite && file.exists(filename)) {
+      if (alsoload) {
+        load(filename)
+      }
+    } else {
+      print(paste(id, filename))
+      x_dir <- dirname(x)
+      x_poolind <- which(unlist(lapply(poolmats, function(y) y$Dir == x_dir)))
+      if(any(x_poolind)) {
+        # x_pool <- poolmats[[x_poolind]]$Pool
+        DynamicsFunction <- poolmats[[x_poolind]]$DynamicsFunction
+        InteractionMatrices <- poolmats[[x_poolind]]$InteractionMatrices
+      } else {
+        warning(paste("Pool not found, skipping", id, filename)); next()
+      }
 
-    # So now Diversity contains summary statistics, presence/absence values,
-    # and simulation metadata.
-    save(EigData, file = filename)
+      # Load result to analyse.
+      loaded <- load(x) # names
+      stopifnot(length(loaded) == 1)
+      loaded <- (get(loaded)) # objects
+      # Note: after patch effects, rather than before
+      if ("EffectiveReproductionRate" %in% names(loaded$Ellipsis$Affinity)) {
+        rprime <- loaded$Ellipsis$Affinity$EffectiveReproductionRate
+      } else if ("EffectiveReproductionRateIntervention" %in% names(loaded$Ellipsis$Affinity)) {
+        rprime <- loaded$Ellipsis$Affinity$EffectiveReproductionRateIntervention
+      } else {
+        warning("Reproduction Rate rprime not found."); next()
+      }
+
+      # Different handling is needed depending on if arguments are functions.
+      # (Could functionalise the calls, but that introduces overhead on each call.)
+      if (is.function(rprime)) {
+        # Calculate rprime using Parms$Patch (i.e., within patch)
+        if (is.function(InteractionMatrices$Mats[[1]])) {
+          # Calculate and combine interaction matrices on the fly.
+          PerCapitaDynamics <- DynamicsFunction(
+            rprime,
+            function(t, y, parms) {
+              Matrix::bdiag(lapply(
+                InteractionMatrices$Mats,
+                function(matfunc) {matfunc(t, y, parms)}
+              ))
+            },
+            loaded$NumEnvironments
+          )
+        }
+        else {
+          # Just combine the interaction matrices.
+          PerCapitaDynamics <- DynamicsFunction(
+            rprime,
+            Matrix::bdiag(InteractionMatrices$Mats),
+            loaded$NumEnvironments
+          )
+        }
+      } else {
+        # Treat rprime as constant and explicitly calculated.
+        if (is.function(InteractionMatrices$Mats[[1]])) {
+          # Calculate and combine interaction matrices on the fly.
+          PerCapitaDynamics <- DynamicsFunction(
+            rprime,
+            function(t, y, parms) {
+              Matrix::bdiag(lapply(
+                InteractionMatrices$Mats,
+                function(matfunc) {matfunc(t, y, parms)}
+              ))
+            }
+          )
+        }
+        else {
+          # Just combine the interaction matrices.
+          PerCapitaDynamics <- DynamicsFunction(
+            rprime,
+            Matrix::bdiag(InteractionMatrices$Mats)
+          )
+        }
+      }
+
+      EigData <- list(
+        EigenData = with(loaded, checkAllDominantEigenvalues(
+          Abundance = Abundance,                            # Inside loaded
+          Events = Events,                                  # Inside loaded
+          PerCapitaDynamics = PerCapitaDynamics,            # Assembled from poolmats
+          DispersalMatrix = Parameters$DispersalMatrix,     # Inside loaded
+          MaxChangeSize = 1e-6 # How much change is "too much" to be @ f.p.
+        )),
+        EigenInteraction = max(abs(unlist(lapply(
+          poolmats[[x_poolind]]$InteractionMatrices$Mats,
+          eigen, only.values = TRUE
+        )))),
+        CharacteristicRate = 1/loaded$ReactionTime, # Should match EigenInteraction.
+        ReproductionRate = poolmats[[x_poolind]]$Pool$ReproductionRate, # orig rs
+        Ellipsis = loaded$Ellipsis
+      )
+
+      if ("ParentRun" %in% names(EigData$Ellipsis))
+        EigData$Ellipsis$GrandparentRun <- EigData$Ellipsis$ParentRun
+      EigData$Ellipsis$ParentRun <- x
+
+      # So now Diversity contains summary statistics, presence/absence values,
+      # and simulation metadata.
+      save(EigData, file = filename)
+    }
+    gc()
+    return(EigData)
   }
+
   gc()
-  return(EigData)
+
+  if (alsoload) {
+    EigenData <- tidytable::bind_rows(lapply(EigenData, function(ED) {
+      # Collect the identifying information for any analyses that we want to do.
+      id <- strsplit(
+        strsplit(
+          # Remove .RData.
+          strsplit(basename(ED$Ellipsis$ParentRun), ".", fixed = TRUE)[[1]][1],
+          # Remove TSTS_Type and split seeds off.
+          "_", fixed = TRUE)[[1]][-c(1:2)],
+        # Separate out the id values.
+        "-", fixed = TRUE
+      )
+
+      if (length(id) < 3) {
+        # I.e., no intervention.
+        id[[3]] <- rep(NA, 4)
+        id[[4]] <- rep(NA, 2)
+      }
+
+      # We need to collect the data.frame of the eigendata, its types, and IDs.
+      # Note that all values should be pure rates at this point:
+      #   Eigenvalues have units equal to those of the entries of the Jacobian.
+      #     In this case, as in the introduction, they are 1/time.
+      #   Reproduciton rates are rates 1/time (multiply by abundance in odes).
+      #   And Intrinsic is 1/(population*time), so we multiple by unit abundance.
+      with(ED, tidytable::bind_rows(
+        EigenData |> tidytable::mutate(
+          Type = "Eigenvalue"
+        ) |> tidytable::rename(
+          Timescale = Eigenvalue
+        ),
+        data.frame(
+          Time = NA, Row = NA, MaxChange = NA,
+          Timescale = ReproductionRate,
+          Type = "Intrinsic"
+        ),
+        data.frame(
+          Time = NA, Row = NA, MaxChange = NA,
+          Timescale = EigenInteraction,
+          Type = "InteractionMatrix"
+        )
+      ) |> tidytable::mutate(
+        PoolPatch = id[[1]][1],
+        PoolPatchSeed = id[[2]][1],
+        Interactions = id[[1]][2],
+        InteractionsSeed = id[[2]][2],
+        Events = id[[1]][3],
+        EventsSeed = id[[2]][3],
+        InitialConditions = id[[1]][4],
+        InitialConditionsSeed = id[[2]][4],
+        Dispersal = id[[1]][5],
+        NicheDistance = id[[1]][6],
+        SpeciesAffinity = id[[1]][7],
+        SpeciesAffinitySeed = id[[2]][5],
+        PatchAffinity = id[[1]][8],
+        PatchAffinitySeed = id[[2]][6],
+        InterventionPatchType = id[[3]][1],
+        InterventionPatchSeed = id[[4]][1],
+        InterventionTimeType = id[[3]][2],
+        InterventionTimeSeed = id[[4]][2],
+        InterventionDispersal = id[[3]][3],
+        InterventionNicheDistance = id[[3]][4],
+        CarryingCapacityType =
+          if (is.function(Ellipsis$Affinity$EffectiveReproductionRate)) {
+            "Basal"
+          } else {
+            "None"
+          },
+        CarryingCapacity =
+          if (is.function(Ellipsis$Affinity$EffectiveReproductionRate)) {
+            environment(
+              Ellipsis$Affinity$EffectiveReproductionRate
+            )$logisticCarryingCapacity$Basal
+          } else {
+            NA
+          }
+      ))
+    }))
+
+    save(EigenData, file = paste0(
+      "EigenData10a1_", chunks[chunk], "_", chunks[chunk+1]-1 ,".RData"
+    ))
+  }
+
+  gc()
 }
 
 if (cores > 1)
   parallel::stopCluster(clust)
 
-gc()
-
-if (alsoload) {
-  EigenData <- tidytable::bind_rows(lapply(EigenData, function(ED) {
-    # Collect the identifying information for any analyses that we want to do.
-    id <- strsplit(
-      strsplit(
-        # Remove .RData.
-        strsplit(basename(ED$Ellipsis$ParentRun), ".", fixed = TRUE)[[1]][1],
-        # Remove TSTS_Type and split seeds off.
-        "_", fixed = TRUE)[[1]][-c(1:2)],
-      # Separate out the id values.
-      "-", fixed = TRUE
-    )
-
-    if (length(id) < 3) {
-      # I.e., no intervention.
-      id[[3]] <- rep(NA, 4)
-      id[[4]] <- rep(NA, 2)
-    }
-
-    # We need to collect the data.frame of the eigendata, its types, and IDs.
-    # Note that all values should be pure rates at this point:
-    #   Eigenvalues have units equal to those of the entries of the Jacobian.
-    #     In this case, as in the introduction, they are 1/time.
-    #   Reproduciton rates are rates 1/time (multiply by abundance in odes).
-    #   And Intrinsic is 1/(population*time), so we multiple by unit abundance.
-    with(ED, tidytable::bind_rows(
-      EigenData |> tidytable::mutate(
-        Type = "Eigenvalue"
-      ) |> tidytable::rename(
-        Timescale = Eigenvalue
-      ),
-      data.frame(
-        Time = NA, Row = NA, MaxChange = NA,
-        Timescale = ReproductionRate,
-        Type = "Intrinsic"
-      ),
-      data.frame(
-        Time = NA, Row = NA, MaxChange = NA,
-        Timescale = EigenInteraction,
-        Type = "InteractionMatrix"
-      )
-    ) |> tidytable::mutate(
-      PoolPatch = id[[1]][1],
-      PoolPatchSeed = id[[2]][1],
-      Interactions = id[[1]][2],
-      InteractionsSeed = id[[2]][2],
-      Events = id[[1]][3],
-      EventsSeed = id[[2]][3],
-      InitialConditions = id[[1]][4],
-      InitialConditionsSeed = id[[2]][4],
-      Dispersal = id[[1]][5],
-      NicheDistance = id[[1]][6],
-      SpeciesAffinity = id[[1]][7],
-      SpeciesAffinitySeed = id[[2]][5],
-      PatchAffinity = id[[1]][8],
-      PatchAffinitySeed = id[[2]][6],
-      InterventionPatchType = id[[3]][1],
-      InterventionPatchSeed = id[[4]][1],
-      InterventionTimeType = id[[3]][2],
-      InterventionTimeSeed = id[[4]][2],
-      InterventionDispersal = id[[3]][3],
-      InterventionNicheDistance = id[[3]][4],
-      CarryingCapacityType =
-        if (is.function(Ellipsis$Affinity$EffectiveReproductionRate)) {
-          "Basal"
-        } else {
-          "None"
-        },
-      CarryingCapacity =
-        if (is.function(Ellipsis$Affinity$EffectiveReproductionRate)) {
-          environment(
-            Ellipsis$Affinity$EffectiveReproductionRate
-            )$logisticCarryingCapacity$Basal
-        } else {
-          NA
-        }
-    ))
-  }))
-
-  save(EigenData, file = "EigenData10a1_flat.RData")
-}
