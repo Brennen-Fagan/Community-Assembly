31f28aed2b58614046dbb1751f40551d17d42a48
Brennen Fagan <Brennen.Fagan@york.ac.uk>
2026-03-19 11:05:15 +0000

    Update TimeSpaceAndTimeSeries-10i-PreparationsRichness.R
    Small typo

--- experiments/FaganEtAl2025/TimeSpaceAndTimeSeries-10i-PreparationsRichness.R
+++ experiments/FaganEtAl2025/TimeSpaceAndTimeSeries-10i-PreparationsRichness.R
@@ -2,7 +2,7 @@
 source("TimeSpaceAndTimeSeries-10h-PlotPreparations.R")
 
 if (!exists("diversitiesRichness")) {
-  paste0("diversitiesFlattened10a1_", dateSwitch, "_Richness.RData"))
+  load(paste0("diversitiesFlattened10a1_", dateSwitch, "_Richness.RData"))
   # load("diversitiesFlattened10a1_2025-09-04_Richness.RData")
 
   diversitiesRichness <- diversitiesRichness |> changePreferencesLevels(
